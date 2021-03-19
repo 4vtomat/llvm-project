8c93aa02965c9d3998c4bd79ba9bb407478e0a92
Monk Chiang <monk.chiang@sifive.com>
2021-03-19 15:45:41 +0800

    [SiFive] Avoid rv32 use large model option.

--- clang/lib/Driver/ToolChains/Arch/RISCV.cpp
+++ clang/lib/Driver/ToolChains/Arch/RISCV.cpp
@@ -64,6 +64,12 @@ void riscv::getRISCVTargetFeatures(const Driver &D, const llvm::Triple &Triple,
                                    std::vector<StringRef> &Features) {
   StringRef MArch = getRISCVArch(Args, Triple);
 
+  if (Args.getLastArgValue(options::OPT_mcmodel_EQ).equals_insensitive("large") &&
+      (Triple.getArch() == llvm::Triple::riscv32)) {
+    D.Diag(diag::err_drv_unsupported_opt_for_target)
+        << "-mcmodel=compact" << "riscv32";
+  }
+
   if (!getArchFeatures(D, MArch, Features, Args))
     return;
 
