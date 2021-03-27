ab0ae01857f43e05a6b6463b4b4178d782032c9e
Craig Topper <craig.topper@sifive.com>
2021-03-26 17:32:10 -0700

    [SiFive][RISCV] Add Zba and Zbb extensions to sifive-viu75.

--- clang/test/Driver/riscv-cpus.c
+++ clang/test/Driver/riscv-cpus.c
@@ -161,6 +161,6 @@
 // RUN: %clang -target riscv64 -### -c %s 2>&1 -mcpu=sifive-viu75 | FileCheck -check-prefix=MCPU-SIFIVE-VIU75 %s
 // MCPU-SIFIVE-VIU75: "-nostdsysteminc" "-target-cpu" "sifive-viu75"
 // MCPU-SIFIVE-VIU75-SAME: "-target-feature" "+m" "-target-feature" "+a" "-target-feature" "+f" "-target-feature" "+d"
-// MCPU-SIFIVE-VIU75-SAME: "-target-feature" "+c" "-target-feature" "+v" "-target-feature" "+zfh"
+// MCPU-SIFIVE-VIU75-SAME: "-target-feature" "+c" "-target-feature" "+v" "-target-feature" "+zfh" "-target-feature" "+zba" "-target-feature" "+zbb"
 // MCPU-SIFIVE-VIU75-SAME: "-target-feature" "+64bit"
 // MCPU-SIFIVE-VIU75-SAME: "-target-abi" "lp64d"
