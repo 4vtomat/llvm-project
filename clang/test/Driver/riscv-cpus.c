b411eefc0764ff163eb056501920fc1f037e71d4
Kito Cheng <kito.cheng@sifive.com>
2021-06-09 15:45:28 +0800

    [RISCV][SiFive] Add dummy option sifive-7m-series to -mtune list.
    - It's just a placeholder, we should implement that in future.

--- clang/test/Driver/riscv-cpus.c
+++ clang/test/Driver/riscv-cpus.c
@@ -45,6 +45,12 @@
 // RUN: %clang --target=riscv64 -### -c %s 2>&1 -mtune=sifive-7-series | FileCheck -check-prefix=MTUNE-SIFIVE7-SERIES-64 %s
 // MTUNE-SIFIVE7-SERIES-64: "-tune-cpu" "sifive-7-rv64"
 
+// RUN: %clang -target riscv32 -### -c %s 2>&1 -mtune=sifive-7m-series | FileCheck -check-prefix=MTUNE-SIFIVE7M-SERIES-32 %s
+// MTUNE-SIFIVE7M-SERIES-32: "-tune-cpu" "sifive-7-rv32"
+
+// RUN: %clang -target riscv64 -### -c %s 2>&1 -mtune=sifive-7m-series | FileCheck -check-prefix=MTUNE-SIFIVE7M-SERIES-64 %s
+// MTUNE-SIFIVE7M-SERIES-64: "-tune-cpu" "sifive-7-rv64"
+
 // mcpu with default march
 // RUN: %clang --target=riscv64 -### -c %s 2>&1 -mcpu=sifive-e20 | FileCheck -check-prefix=MCPU-SIFIVE-E20 %s
 // MCPU-SIFIVE-E20: "-nostdsysteminc" "-target-cpu" "sifive-e20"
