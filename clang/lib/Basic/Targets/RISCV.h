7f2b63ca6d65c1755e790d0a8535ce1f5e8379fd
Kai Wang <kai.wang@sifive.com>
2021-08-18 10:43:48 +0800

    [SiFive][RISCV] Promote alignment for loading/storing large array. (#162)
    As discussed in SCT-635.
    
    Define "large array" as the size >= 64 bits and promote the alignment to
    64 bits for RISCV64.

--- clang/lib/Basic/Targets/RISCV.h
+++ clang/lib/Basic/Targets/RISCV.h
@@ -136,6 +136,8 @@ class LLVM_LIBRARY_VISIBILITY RISCV64TargetInfo : public RISCVTargetInfo {
 public:
   RISCV64TargetInfo(const llvm::Triple &Triple, const TargetOptions &Opts)
       : RISCVTargetInfo(Triple, Opts) {
+    LargeArrayMinWidth = 64; // SIFIVE
+    LargeArrayAlign = 64; // SIFIVE
     LongWidth = LongAlign = PointerWidth = PointerAlign = 64;
     IntMaxType = Int64Type = SignedLong;
     resetDataLayout("e-m:e-p:64:64-i64:64-i128:128-n64-S128");
