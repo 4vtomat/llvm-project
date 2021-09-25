79d4f93bc75dbb123ce1e3dd1df1e893f26717e3
Zakk Chen <zakk.chen@sifive.com>
2021-09-24 21:19:11 -0700

    [SiFive][RISCV] Add mtune=sifive-p600-series/sifive-p600-series.
    https://sifive.atlassian.net/wiki/spaces/T/pages/2088698638/SiFive+Toolchain+2021.09+ReleasFeatures
    
    sifive-p600-series alias to sifive-9-series.
    sifive-p500-series alias to sifive-8-series.

--- clang/test/Misc/target-invalid-cpu-note.c
+++ clang/test/Misc/target-invalid-cpu-note.c
@@ -89,8 +89,8 @@
 
 // RUN: not %clang_cc1 -triple riscv32 -tune-cpu not-a-cpu -fsyntax-only %s 2>&1 | FileCheck %s --check-prefix TUNE-RISCV32
 // TUNE-RISCV32: error: unknown target CPU 'not-a-cpu'
-// TUNE-RISCV32-NEXT: note: valid target CPU values are: generic-rv32, rocket-rv32, sifive-6-rv32, sifive-7-rv32, sifive-e20, sifive-e21, sifive-e24, sifive-e31, sifive-e34, sifive-e66, sifive-e76, sifive-e76m, sifive-8-rv32, generic, rocket, sifive-6-series, sifive-7-series, sifive-7m-series, sifive-8-series, sifive-9-series{{$}}
+// TUNE-RISCV32-NEXT: note: valid target CPU values are: generic-rv32, rocket-rv32, sifive-6-rv32, sifive-7-rv32, sifive-e20, sifive-e21, sifive-e24, sifive-e31, sifive-e34, sifive-e66, sifive-e76, sifive-e76m, sifive-8-rv32, generic, rocket, sifive-6-series, sifive-7-series, sifive-7m-series, sifive-8-series, sifive-9-series, sifive-p500-series, sifive-p600-series{{$}}
 
 // RUN: not %clang_cc1 -triple riscv64 -tune-cpu not-a-cpu -fsyntax-only %s 2>&1 | FileCheck %s --check-prefix TUNE-RISCV64
 // TUNE-RISCV64: error: unknown target CPU 'not-a-cpu'
-// TUNE-RISCV64-NEXT: note: valid target CPU values are: generic-rv64, rocket-rv64, sifive-6-rv64, sifive-7-rv64, sifive-s21, sifive-s51, sifive-s54, sifive-s66, sifive-s76, sifive-s76m, sifive-u54, sifive-u64, sifive-u74, sifive-u74m, sifive-8-rv64, sifive-x280, sifive-x280n, sifive-p270, sifive-p550, sifive-p650, generic, rocket, sifive-6-series, sifive-7-series, sifive-7m-series, sifive-8-series, sifive-9-series{{$}}
+// TUNE-RISCV64-NEXT: note: valid target CPU values are: generic-rv64, rocket-rv64, sifive-6-rv64, sifive-7-rv64, sifive-s21, sifive-s51, sifive-s54, sifive-s66, sifive-s76, sifive-s76m, sifive-u54, sifive-u64, sifive-u74, sifive-u74m, sifive-8-rv64, sifive-x280, sifive-x280n, sifive-p270, sifive-p550, sifive-p650, generic, rocket, sifive-6-series, sifive-7-series, sifive-7m-series, sifive-8-series, sifive-9-series, sifive-p500-series, sifive-p600-series{{$}}
