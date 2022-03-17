ac74304606c17a9548065842801fc75fa740368e
Craig Topper <craig.topper@sifive.com>
2022-03-17 14:16:40 -0700

    Revert "[SiFive][RISCV][MC] Added hypervisor 0.6.2 instructions (priv 1.12-draft-20210808)."
    This reverts commit 66eb1574f9282420f56e5f73f8259dfe941ad7b0.
    
    This has been fixed upstream and didn't merge correctly.

--- llvm/test/MC/RISCV/rvi-aliases-valid.s
+++ llvm/test/MC/RISCV/rvi-aliases-valid.s
@@ -269,20 +269,6 @@ hfence.vvma
 # CHECK-S-OBJ: hfence.vvma a0
 hfence.vvma a0
 
-# CHECK-S-OBJ-NOALIAS: hfence.vvma zero, zero
-# CHECK-S-OBJ: hfence.vvma
-hfence.vvma
-# CHECK-S-OBJ-NOALIAS: hfence.vvma a0, zero
-# CHECK-S-OBJ: hfence.vvma a0
-hfence.vvma a0
-
-# CHECK-S-OBJ-NOALIAS: hfence.gvma zero, zero
-# CHECK-S-OBJ: hfence.gvma
-hfence.gvma
-# CHECK-S-OBJ-NOALIAS: hfence.gvma a0, zero
-# CHECK-S-OBJ: hfence.gvma a0
-hfence.gvma a0
-
 # The following aliases are accepted as input but the canonical form
 # of the instruction will always be printed.
 # CHECK-S-OBJ-NOALIAS: addi a2, a3, 4
