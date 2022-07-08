483877cabf017c6971b1b81a7e783e6be2391366
Craig Topper <craig.topper@sifive.com>
2022-07-07 21:18:11 -0700

    [SiFive][RISCV] Increase the cost of fixed vector reduction
    Prevents non-profitable vectorization in 464.h264ref

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
@@ -655,7 +655,10 @@ RISCVTTIImpl::getArithmeticReductionCost(unsigned Opcode, VectorType *Ty,
     return (LT.first - 1) + (ISD == ISD::AND ? 3 : 2);
 
   // IR Reduction is composed by two vmv and one rvv reduction instruction.
-  InstructionCost BaseCost = 2;
+#if SIFIVE_CUSTOMIZATION
+  // The vector to scalar move is expensive on x280, give it more cost.
+  InstructionCost BaseCost = 4;
+#endif // SIFIVE_CUSTOMIZATION
   unsigned VL = cast<FixedVectorType>(Ty)->getNumElements();
   if (TTI::requiresOrderedReduction(FMF))
     return (LT.first - 1) + BaseCost + VL;
