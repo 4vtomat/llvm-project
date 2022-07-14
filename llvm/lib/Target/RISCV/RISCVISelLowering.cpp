906ce1c4ab6ce101af8c4fa8b65ab13af97b5f42
4vtomat <brandon.wu@sifive.com>
2022-07-14 07:30:16 -0700

    [SiFive] Fold vp.reverse with unit-stride load into a -1 strided load

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -1138,6 +1138,7 @@ RISCVTargetLowering::RISCVTargetLowering(const TargetMachine &TM,
     setTargetDAGCombine({ISD::FCOPYSIGN, ISD::MGATHER, ISD::MSCATTER,
                          ISD::VP_GATHER, ISD::VP_SCATTER, ISD::SRA, ISD::SRL,
                          ISD::SHL, ISD::STORE,
+                         ISD::EXPERIMENTAL_VP_REVERSE, //SIFIVE
                          ISD::SPLAT_VECTOR,        // SIFIVE
                          ISD::INTRINSIC_WO_CHAIN,  // SIFIVE
                          ISD::VECTOR_SHUFFLE,      // SIFIVE
@@ -10046,6 +10047,62 @@ performEXTRACT_VECTOR_ELTCombine(SDNode *N, SelectionDAG &DAG,
   return Load;
 }
 
+static SDValue performVP_REVERSECombine(SDNode *N, SelectionDAG &DAG,
+                                        const RISCVSubtarget &Subtarget) {
+  // Fold:
+  //    vp.reverse(vp.load(ADDR, MASK)) -> vp.strided.load(ADDR, -1, MASK)
+
+  // Check if its first operand is a vp.load.
+  if (auto *VPLoad = dyn_cast<VPLoadSDNode>(N->getOperand(0))) {
+    EVT LoadVT = VPLoad->getValueType(0);
+    // We do not have a strided_load version for masks, and the evl of vp.reverse
+    // and vp.load should always be the same.
+    if (!LoadVT.getVectorElementType().isByteSized() ||
+        N->getOperand(2) != VPLoad->getVectorLength() ||
+        !N->getOperand(0).hasOneUse())
+      return SDValue();
+
+    // Check if the mask of outer vp.reverse are all 1's.
+    if (!isOneOrOneSplat(N->getOperand(1)))
+      return SDValue();
+
+    SDValue LoadMask = VPLoad->getMask();
+    // If Mask is not all 1's, try to replace the mask if it's opcode
+    // is EXPERIMENTAL_VP_REVERSE and it's operand can be directly extracted.
+    if (!isOneOrOneSplat(LoadMask)) {
+      // Check if the mask of vp.reverse in vp.load are all 1's and
+      // the length of mask is same as evl.
+      if (LoadMask.getOpcode() != ISD::EXPERIMENTAL_VP_REVERSE ||
+          !isOneOrOneSplat(LoadMask.getOperand(1)) ||
+          LoadMask.getOperand(2) != VPLoad->getVectorLength())
+        return SDValue();
+      LoadMask = LoadMask.getOperand(0);
+    }
+
+    // Base = LoadAddr + (NumElem - 1) * ElemWidthByte
+    SDLoc DL(N);
+    MVT XLenVT = Subtarget.getXLenVT();
+    SDValue NumElem = VPLoad->getVectorLength();
+    uint64_t ElemWidthByte = VPLoad->getValueType(0).getScalarSizeInBits() / 8;
+
+    SDValue Temp1 = DAG.getNode(ISD::SUB, DL, XLenVT, NumElem,
+                                DAG.getConstant(1, DL, XLenVT));
+    SDValue Temp2 = DAG.getNode(ISD::MUL, DL, XLenVT, Temp1,
+                                DAG.getConstant(ElemWidthByte, DL, XLenVT));
+    SDValue Base = DAG.getNode(ISD::ADD, DL, XLenVT, VPLoad->getBasePtr(), Temp2);
+    SDValue Stride = DAG.getConstant(0 - ElemWidthByte, DL, XLenVT);
+    SDValue Ret = DAG.getStridedLoadVP(LoadVT, DL, VPLoad->getChain(),
+                                       Base, Stride, LoadMask, VPLoad->getVectorLength(),
+                                       VPLoad->getMemOperand(), VPLoad->isExpandingLoad());
+
+    DAG.ReplaceAllUsesOfValueWith(SDValue(VPLoad, 1), Ret.getValue(1));
+
+    return Ret;
+  }
+
+  return SDValue();
+}
+
 static SDValue combineVECTOR_SHUFFLEToVnsrl(SDNode *N, SelectionDAG &DAG,
                                             const RISCVSubtarget &Subtarget) {
   // t34: v8i8 = extract_subvector t11, Constant:i64<0>
@@ -10974,6 +11031,8 @@ SDValue RISCVTargetLowering::PerformDAGCombine(SDNode *N,
   }
   case ISD::EXTRACT_VECTOR_ELT:
     return performEXTRACT_VECTOR_ELTCombine(N, DAG, Subtarget);
+  case ISD::EXPERIMENTAL_VP_REVERSE:
+    return performVP_REVERSECombine(N, DAG, Subtarget);
 #endif // SIFIVE_CUSTOMIZATION
   case ISD::BITCAST: {
     assert(Subtarget.useRVVForFixedLengthVectors());
