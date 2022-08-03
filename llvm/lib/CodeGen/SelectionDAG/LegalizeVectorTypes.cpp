bbc69ab2e570fd9a365487dfc415b5f238ce43f3
Craig Topper <craig.topper@sifive.com>
2022-08-02 17:42:28 -0700

    [SiFive][LegalizeTypes] Remove dead code.

--- llvm/lib/CodeGen/SelectionDAG/LegalizeVectorTypes.cpp
+++ llvm/lib/CodeGen/SelectionDAG/LegalizeVectorTypes.cpp
@@ -3573,45 +3573,6 @@ SDValue DAGTypeLegalizer::SplitVecOp_VSETCC(SDNode *N) {
   return DAG.getNode(ExtendCode, DL, N->getValueType(0), Con);
 }
 
-#if SIFIVE_CUSTOMIZATION
-SDValue DAGTypeLegalizer::SplitVecOp_VP_SETCC(SDNode *N) {
-  assert(N->getValueType(0).isVector() &&
-         N->getOperand(0).getValueType().isVector() &&
-         "Operand types must be vectors");
-  // The result has a legal vector type, but the input needs splitting.
-  SDValue Lo0, Hi0, Lo1, Hi1, LoRes, HiRes;
-  SDValue MaskLo, MaskHi;
-  SDValue EVLLo, EVLHi;
-  SDLoc DL(N);
-  GetSplitVector(N->getOperand(0), Lo0, Hi0);
-  GetSplitVector(N->getOperand(1), Lo1, Hi1);
-  if (getTypeAction(N->getOperand(3).getValueType()) == TargetLowering::TypeSplitVector)
-    GetSplitVector(N->getOperand(3), MaskLo, MaskHi);
-  else
-    std::tie(MaskLo, MaskHi) = DAG.SplitVectorOperand(N, 3);
-
-  std::tie(EVLLo, EVLHi) =
-      DAG.SplitEVL(N->getOperand(4), N->getValueType(0), DL);
-
-  auto PartEltCnt = Lo0.getValueType().getVectorElementCount();
-
-  LLVMContext &Context = *DAG.getContext();
-  EVT PartResVT = EVT::getVectorVT(Context, MVT::i1, PartEltCnt);
-  EVT WideResVT = EVT::getVectorVT(Context, MVT::i1, PartEltCnt*2);
-
-  LoRes = DAG.getNode(ISD::VP_SETCC, DL, PartResVT, Lo0, Lo1, N->getOperand(2),
-                      MaskLo, EVLLo);
-  HiRes = DAG.getNode(ISD::VP_SETCC, DL, PartResVT, Hi0, Hi1, N->getOperand(2),
-                      MaskHi, EVLHi);
-  SDValue Con = DAG.getNode(ISD::CONCAT_VECTORS, DL, WideResVT, LoRes, HiRes);
-
-  EVT OpVT = N->getOperand(0).getValueType();
-  ISD::NodeType ExtendCode =
-      TargetLowering::getExtendForContent(TLI.getBooleanContents(OpVT));
-  return DAG.getNode(ExtendCode, DL, N->getValueType(0), Con);
-}
-#endif // SIFIVE_CUSTOMIZATION
-
 SDValue DAGTypeLegalizer::SplitVecOp_FP_ROUND(SDNode *N) {
   // The result has a legal vector type, but the input needs splitting.
   EVT ResVT = N->getValueType(0);
