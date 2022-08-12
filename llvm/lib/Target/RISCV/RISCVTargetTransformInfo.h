97c52165781c8b2e0da04e150b866cb1fbc7fa47
ShihPo Hung <shihpo.hung@sifive.com>
2022-08-12 01:57:41 -0700

    [SiFive][Merge] Fix for overloaded method 'getVectorInstrCost'

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
@@ -371,6 +371,7 @@ public:
 #if SIFIVE_CUSTOMIZATION
   unsigned getInliningThresholdMultiplier();
 
+  using BaseT::getVectorInstrCost;
   InstructionCost getVectorInstrCost(unsigned Opcode, Type *Val,
                                      unsigned Index);
 
