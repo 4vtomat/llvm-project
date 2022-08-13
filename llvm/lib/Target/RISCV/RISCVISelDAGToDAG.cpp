61adefd186021a531d82f01adcb7a494ebd46866
Craig Topper <craig.topper@sifive.com>
2022-08-12 21:08:22 -0700

    [SiFive][RISCV] Fold doPeepholeLUIADDI into the main loop of PostprocessISelDAG.
    This was a separate pass over the DAG to avoid interfering with
    doPeepholeLoadStoreADDI. But doPeepholeLoadStoreADDI no longer
    exists that so that is no longer an issue.

--- llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
+++ llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
@@ -149,14 +149,12 @@ void RISCVDAGToDAGISel::PostprocessISelDAG() {
 
     MadeChange |= doPeepholeSExtW(N);
     MadeChange |= doPeepholeMaskedRVV(N);
+#if SIFIVE_CUSTOMIZATION
+    MadeChange |= doPeepholeLUIADDI(N);
+#endif // SIFIVE_CUSTOMIZATION
   }
 
   CurDAG->setRoot(Dummy.getValue());
-
-#if SIFIVE_CUSTOMIZATION
-  // Done as a separate pass to avoid interfering with doPeepholeLoadStoreADDI.
-  MadeChange |= doPeepholeLUIADDI();
-#endif // SIFIVE_CUSTOMIZATION
 
   if (MadeChange)
     CurDAG->RemoveDeadNodes();
@@ -2688,60 +2686,48 @@ bool RISCVDAGToDAGISel::doPeepholeMaskedRVV(SDNode *N) {
 }
 
 #if SIFIVE_CUSTOMIZATION
-bool RISCVDAGToDAGISel::doPeepholeLUIADDI() {
+bool RISCVDAGToDAGISel::doPeepholeLUIADDI(SDNode *N) {
+  unsigned Opc = N->getMachineOpcode();
+  if (Opc != RISCV::ADDI && Opc != RISCV::ADDIW)
+    return false;
+
+  if (!isa<ConstantSDNode>(N->getOperand(1)))
+    return false;
+
   if (!Subtarget->hasLUIADDIFusion() && !Subtarget->usePseudoLIsimm32())
     return false;
 
-  SelectionDAG::allnodes_iterator Position = CurDAG->allnodes_end();
+  SDValue Src = N->getOperand(0);
+  if (!Src.isMachineOpcode() || Src.getMachineOpcode() != RISCV::LUI ||
+      !Src.hasOneUse() || !isa<ConstantSDNode>(Src.getOperand(0)))
+    return false;
 
-  bool MadeChange = false;
-  while (Position != CurDAG->allnodes_begin()) {
-    SDNode *N = &*--Position;
-    // Skip dead nodes and any non-machine opcodes.
-    if (N->use_empty() || !N->isMachineOpcode())
-      continue;
+  uint64_t Imm = cast<ConstantSDNode>(Src.getOperand(0))->getZExtValue();
+  assert(isUInt<20>(Imm) && "Unexpected immediate");
 
-    unsigned Opc = N->getMachineOpcode();
-    if (Opc != RISCV::ADDI && Opc != RISCV::ADDIW)
-      continue;
+  // Emulate LUI.
+  Imm <<= 12;
+  Imm = SignExtend64(Imm, 32);
 
-    if (!isa<ConstantSDNode>(N->getOperand(1)))
-      continue;
+  // Add the low immediate from ADDI/ADDIW.
+  int64_t LoImm = cast<ConstantSDNode>(N->getOperand(1))->getSExtValue();
+  assert(isInt<12>(LoImm) && "Unexpected immediate");
+  Imm += LoImm;
 
-    SDValue Src = N->getOperand(0);
-    if (!Src.isMachineOpcode() || Src.getMachineOpcode() != RISCV::LUI ||
-        !Src.hasOneUse() || !isa<ConstantSDNode>(Src.getOperand(0)))
-      continue;
-
-    uint64_t Imm = cast<ConstantSDNode>(Src.getOperand(0))->getZExtValue();
-    assert(isUInt<20>(Imm) && "Unexpected immediate");
-
-    // Emulate LUI.
-    Imm <<= 12;
+  // If this is an ADDIW or RV32, sign extend the result.
+  if (Opc == RISCV::ADDIW || !Subtarget->is64Bit())
     Imm = SignExtend64(Imm, 32);
 
-    // Add the low immediate from ADDI/ADDIW.
-    int64_t LoImm = cast<ConstantSDNode>(N->getOperand(1))->getSExtValue();
-    assert(isInt<12>(LoImm) && "Unexpected immediate");
-    Imm += LoImm;
+  // Final constant should be simm32.
+  if (!isInt<32>(Imm))
+    return false;
 
-    // If this is an ADDIW or RV32, sign extend the result.
-    if (Opc == RISCV::ADDIW || !Subtarget->is64Bit())
-      Imm = SignExtend64(Imm, 32);
-
-    // Final constant should be simm32.
-    if (!isInt<32>(Imm))
-      continue;
-
-    EVT VT = N->getValueType(0);
-    SDLoc DL(N);
-    SDNode *Result = CurDAG->getMachineNode(RISCV::PseudoLIsimm32, DL, VT,
-                                            CurDAG->getTargetConstant(Imm, DL, VT));
-    ReplaceUses(N, Result);
-    MadeChange = true;
-  }
-
-  return MadeChange;
+  EVT VT = N->getValueType(0);
+  SDLoc DL(N);
+  SDNode *Result = CurDAG->getMachineNode(RISCV::PseudoLIsimm32, DL, VT,
+                                          CurDAG->getTargetConstant(Imm, DL, VT));
+  ReplaceUses(N, Result);
+  return true;
 }
 #endif // SIFIVE_CUSTOMIZATION
 
