6a46e15075884ea2575d1fea0d814d1cbd597d97
Kito Cheng <kito.cheng@sifive.com>
2022-08-31 01:05:02 +0800

    [SiFive][Merge] Update ext. version in testcase

--- llvm/test/MC/RISCV/attribute-arch.s
+++ llvm/test/MC/RISCV/attribute-arch.s
@@ -188,17 +188,16 @@
 # CHECK: attribute      5, "rv32i2p1_zbkb1p0_zbkc1p0_zbkx1p0_zk1p0_zkn1p0_zknd1p0_zkne1p0_zknh1p0_zkr1p0_zkt1p0"
 
 .attribute arch, "rv32izihintntl0p2"
-# CHECK: attribute      5, "rv32i2p0_zihintntl0p2"
+# CHECK: attribute      5, "rv32i2p1_zihintntl0p2"
 
 .attribute arch, "rv32iczihintntl0p2"
-# CHECK: attribute      5, "rv32i2p0_c2p0_zihintntl0p2"
+# CHECK: attribute      5, "rv32i2p1_c2p0_zihintntl0p2"
 
 .attribute arch, "rv32if_zkt1p0_zve32f1p0_zve32x1p0_zvl32b1p0"
-<<<<<<< HEAD
 # CHECK: attribute      5, "rv32i2p1_f2p2_zicsr2p0_zkt1p0_zve32f1p0_zve32x1p0_zvl32b1p0"
 
-.attribute arch, "rv32iv_xsfvqmaccdod_xsfvqmaccqoq"
-# CHECK: attribute      5, "rv32i2p1_f2p2_d2p2_v1p0_zicsr2p0_zve32f1p0_zve32x1p0_zve64d1p0_zve64f1p0_zve64x1p0_zvl128b1p0_zvl256b1p0_zvl32b1p0_zvl64b1p0_xsfvqmaccdod0p1_xsfvqmaccqoq0p1"
+.attribute arch, "rv32izca0p70"
+# CHECK: attribute      5, "rv32i2p1_zca0p70"
 
 .attribute arch, "rv32ifv_xsfvfnrclipxfqf_xsfvfwmaccqqq"
 # CHECK: attribute      5, "rv32i2p1_f2p2_d2p2_v1p0_zicsr2p0_zve32f1p0_zve32x1p0_zve64d1p0_zve64f1p0_zve64x1p0_zvl128b1p0_zvl256b1p0_zvl32b1p0_zvl64b1p0_xsfvfnrclipxfqf0p1_xsfvfwmaccqqq0p1"
@@ -247,9 +246,3 @@
 
 .attribute arch, "rv32i_zve64x"
 # CHECK: attribute      5, "rv32i2p1_zve32x1p0_zve64x1p0_zvl32b1p0_zvl64b1p0"
-=======
-# CHECK: attribute      5, "rv32i2p0_f2p0_zkt1p0_zve32f1p0_zve32x1p0_zvl32b1p0"
-
-.attribute arch, "rv32izca0p70"
-# CHECK: attribute      5, "rv32i2p0_zca0p70"
->>>>>>> 1f5215668a2bbf34a915f0e3a4e2ca65e28915c7
