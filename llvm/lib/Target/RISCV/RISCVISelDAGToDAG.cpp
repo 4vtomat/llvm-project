56b779208afbed595ff858cda1be96ae27ae28b0
Craig Topper <craig.topper@sifive.com>
2022-09-29 21:24:05 -0700

    [SiFive][RISCV] Prevent performCombineVMergeAndVOps from forming cycles in the DAG.
    Fixes SCT-1802. This needs to be upstreamed.

--- llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
+++ llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
@@ -2800,6 +2800,22 @@ bool RISCVDAGToDAGISel::performCombineVMergeAndVOps(SDNode *N, bool IsTA) {
   bool HasChainOp =
       True.getOperand(True.getNumOperands() - 1).getValueType() == MVT::Other;
 
+#if SIFIVE_CUSTOMIZATION
+  // Avoid creating cycles in the DAG.
+  // TODO: Upstream this.
+  if (HasChainOp) {
+    SmallVector<const SDNode *, 4> LoopWorklist;
+    SmallPtrSet<const SDNode *, 16> Visited;
+    LoopWorklist.push_back(False.getNode());
+    LoopWorklist.push_back(Mask.getNode());
+    LoopWorklist.push_back(VL.getNode());
+    if (SDNode *Glued = N->getGluedNode())
+      LoopWorklist.push_back(Glued);
+    if (SDNode::hasPredecessorHelper(True.getNode(), Visited, LoopWorklist))
+      return false;
+  }
+#endif // SIFIVE_CUSTOMIZATION
+
   // Need True has same VL with N.
   unsigned TrueVLIndex = True.getNumOperands() - HasChainOp - 2;
   SDValue TrueVL = True.getOperand(TrueVLIndex);
