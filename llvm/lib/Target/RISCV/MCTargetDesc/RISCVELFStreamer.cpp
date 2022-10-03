99ed1aff734093d4b1e4913ac69a47b3ad4e85ab
eopXD <yueh.ting.chen@gmail.com>
2022-10-03 01:54:50 -0700

    [SiFive] Resolve conflict llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFStreamer.cpp
    After D132262, the function `IsMetadataOrEHFrameSection` is no longer
    used.

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFStreamer.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFStreamer.cpp
@@ -209,20 +209,6 @@ class RISCVELFStreamer : public MCELFStreamer {
 
   static bool requiresFixups(MCContext &C, const MCExpr *Value,
                              const MCExpr *&LHS, const MCExpr *&RHS) {
-<<<<<<< HEAD
-#if SIFIVE_CUSTOMIZATION
-    // SIFIVE: Cherry-picked from upstream
-    auto IsMetadataOrEHFrameSection = [](const MCSection &S) -> bool {
-      // Additionally check .apple_names/.apple_types. They are fixed-size and
-      // do not need fixups. llvm-dwarfdump --apple-names does not process
-      // R_RISCV_{ADD,SUB}32 in them.
-      return S.getKind().isMetadata() || S.getName() == ".eh_frame" ||
-             S.getName() == ".apple_names" || S.getName() == ".apple_types";
-    };
-#endif // SIFIVE_CUSTOMIZATION
-
-=======
->>>>>>> upstream/main
     const auto *MBE = dyn_cast<MCBinaryExpr>(Value);
     if (MBE == nullptr)
       return false;
