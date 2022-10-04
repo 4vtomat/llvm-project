f6dc68d64264d971d585be9524bd421e08a1747c
Han-Kuan Chen <hankuan.chen@sifive.com>
2022-10-04 00:09:13 -0700

    [SiFive][Recode] Fix requireExpand comment.

--- llvm/include/llvm/Transforms/Utils/SiFive_RecodeExpand.h
+++ llvm/include/llvm/Transforms/Utils/SiFive_RecodeExpand.h
@@ -12,8 +12,8 @@ public:
   // O0 requires this.
   static bool isRequired() { return true; }
   // Return true if a Neon intrinsic will be expanded by this pass.
-  // Not all Neon intrinsics is required expand. (e.g., aarch64_neon_frsqrte is
-  // lowered to RISCVISD::FRSQRT7_VL)
+  // Not all Neon intrinsics is required expand. (e.g., aarch64_neon_fmax is
+  // lowered to RISCVISD::FMAXNUM_VL)
   static bool requireExpand(IntrinsicInst *II);
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
 };
