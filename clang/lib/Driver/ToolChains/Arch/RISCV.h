8d665759280bbaa48ff81243a3bdb473e285b742
4vtomat <brandon.wu@sifive.com>
2022-11-06 23:00:22 -0800

    [SiFive][Merge] clang/lib/Driver/ToolChains/Arch/RISCV.h

--- clang/lib/Driver/ToolChains/Arch/RISCV.h
+++ clang/lib/Driver/ToolChains/Arch/RISCV.h
@@ -26,7 +26,7 @@ StringRef getRISCVABI(const llvm::opt::ArgList &Args,
                       const llvm::Triple &Triple);
 StringRef getRISCVArch(const llvm::opt::ArgList &Args,
                        const llvm::Triple &Triple);
-<<<<<<< HEAD
+
 StringRef getRISCVCodeModel(const llvm::opt::ArgList &Args);
 
 #if SIFIVE_CUSTOMIZATION
@@ -35,10 +35,8 @@ void addRISCVTargetLTOArgs(const ToolChain &ToolChain,
                            llvm::opt::ArgStringList &CmdArgs);
 #endif // SIFIVE_CUSTOMIZATION
 
-=======
 std::string getRISCVTargetCPU(const llvm::opt::ArgList &Args,
                               const llvm::Triple &Triple);
->>>>>>> llvm/main
 } // end namespace riscv
 } // namespace tools
 } // end namespace driver
