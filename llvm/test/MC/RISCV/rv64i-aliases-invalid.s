8a614b465877547315e878643d0506760d71b76a
Mel Chen <mel.chen@sifive.com>
2022-12-15 00:57:41 -0800

    [SiFive][Merge] Update MC test case.
    Conflict with:
      [Windows] Canonicalize on 'target=.*-windows'
    
      [Windows] Convert tests to check 'target=<triple>'

--- llvm/test/MC/RISCV/rv64i-aliases-invalid.s
+++ llvm/test/MC/RISCV/rv64i-aliases-invalid.s
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-# UNSUPPORTED: windows
+# UNSUPPORTED: target={{.*-windows.*}}
 # RUN: not llvm-mc -triple=riscv64 --mattr=+zicsr -riscv-no-aliases < %s -o /dev/null 2>&1 | FileCheck %s
 # RUN: not llvm-mc -triple=riscv64 --mattr=+zicsr < %s 2>&1 -o /dev/null | FileCheck %s
-=======
-# UNSUPPORTED: target={{.*-windows.*}}
-# RUN: not llvm-mc -triple=riscv64 -riscv-no-aliases < %s -o /dev/null 2>&1 | FileCheck %s
-# RUN: not llvm-mc -triple=riscv64 < %s 2>&1 -o /dev/null | FileCheck %s
->>>>>>> upstream/main
 
 li t5, 0x10000000000000000 # CHECK: :[[@LINE]]:8: error: unknown operand
 li t4, foo                 # CHECK: :[[@LINE]]:8: error: operand must be a constant 64-bit integer
