0914a1b29863070f5556126975a32346a3342af5
Michael Maitland <michaeltmaitland@gmail.com>
2023-01-09 12:35:29 -0800

    [SiFive][Merge] Fix 5a57ebcc4326cc76d6d0f76d8d03d3e53208de21

--- llvm/unittests/IR/VPIntrinsicTest.cpp
+++ llvm/unittests/IR/VPIntrinsicTest.cpp
@@ -146,15 +146,12 @@ protected:
     Str << " declare <8 x i1> @llvm.vp.icmp.v8i16"
         << "(<8 x i16>, <8 x i16>, metadata, <8 x i1>, i32) ";
 
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
     Str << " declare <8 x i32> @llvm.experimental.vp.reverse.v8i32(<8 x i32>, <8 x i1>, i32) ";
     Str << " declare i32 @llvm.vp.first.v8i1(<8 x i1>, <8 x i1>, i32) ";
 #endif // SIFIVE_CUSTOMIZATION
-=======
     Str << " declare <8 x i16> @llvm.vp.abs.v8i16"
         << "(<8 x i16>, <8 x i1>, i32, i1 immarg) ";
->>>>>>> upstream/main
     Str << " declare <8 x i16> @llvm.vp.bitreverse.v8i16"
         << "(<8 x i16>, <8 x i1>, i32) ";
     Str << " declare <8 x i16> @llvm.vp.bswap.v8i16"
