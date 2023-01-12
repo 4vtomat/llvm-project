c53ce2deffd6b64a9c462fe18437231bc32b89c5
Craig Topper <craig.topper@sifive.com>
2023-01-11 22:30:03 -0800

    [SiFive][RISCV] Remove customization for folding vfmv.f.s+store.
    Upstream implemented this with an isel pattern sometime later.
    Drop our customization which wasn't marked properly.

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -6376,9 +6376,6 @@ SDValue RISCVTargetLowering::LowerINTRINSIC_WO_CHAIN(SDValue Op,
     assert(Op.getValueType() == XLenVT && "Unexpected VT!");
     return DAG.getNode(RISCVISD::VMV_X_S, DL, Op.getValueType(),
                        Op.getOperand(1));
-  case Intrinsic::riscv_vfmv_f_s:
-    return DAG.getNode(RISCVISD::VFMV_F_S, DL, Op.getValueType(),
-                       Op.getOperand(1));
   case Intrinsic::riscv_vmv_v_x:
     return lowerScalarSplat(Op.getOperand(1), Op.getOperand(2),
                             Op.getOperand(3), Op.getSimpleValueType(), DL, DAG,
@@ -12651,8 +12648,8 @@ SDValue RISCVTargetLowering::PerformDAGCombine(SDNode *N,
     auto *Store = cast<StoreSDNode>(N);
     SDValue Val = Store->getValue();
     // Combine store of vmv.x.s to vse with VL of 1.
-    if (Val.getOpcode() == RISCVISD::VMV_X_S ||
-        Val.getOpcode() == RISCVISD::VFMV_F_S) {
+    // FIXME: Support FP.
+    if (Val.getOpcode() == RISCVISD::VMV_X_S) {
       SDValue Src = Val.getOperand(0);
       MVT VecVT = Src.getSimpleValueType();
       EVT MemVT = Store->getMemoryVT();
@@ -15627,7 +15624,6 @@ const char *RISCVTargetLowering::getTargetNodeName(unsigned Opcode) const {
   NODE_NAME_CASE(VFMV_V_F_VL)
   NODE_NAME_CASE(VMV_X_S)
   NODE_NAME_CASE(VMV_S_X_VL)
-  NODE_NAME_CASE(VFMV_F_S)
   NODE_NAME_CASE(VFMV_S_F_VL)
   NODE_NAME_CASE(SPLAT_VECTOR_SPLIT_I64_VL)
   NODE_NAME_CASE(READ_VLENB)
