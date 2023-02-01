32381ece9d40e49f1a073e176d3165ba892881cc
Kolya Panchenko <kolya.panchenko@sifive.com>
2023-02-01 10:50:28 -0800

    [SiFive][Merge] Bumped up LLVM version to 16.9

--- clang/test/Frontend/sarif-diagnostics.cpp
+++ clang/test/Frontend/sarif-diagnostics.cpp
@@ -64,5 +64,7 @@ void f1(t1 x, t1 y) {
 // CHECK: {"enabled":true,"level":"error","rank":50},"fullDescription":{"text":""},"id":"{{[0-9]+}}","name":""},{"defaultConfiguration":
 // CHECK: {"enabled":true,"level":"error","rank":50},"fullDescription":{"text":""},"id":"{{[0-9]+}}","name":""},{"defaultConfiguration":
 // CHECK: {"enabled":true,"level":"error","rank":50},"fullDescription":
-// CHECK: {"text":""},"id":"{{[0-9]+}}","name":""}],"version":"14.9.0"}}}],"version":"2.1.0"}
+// SIFIVE_CUSTOMIZATION
+// CHECK: {"text":""},"id":"{{[0-9]+}}","name":""}],"version":"16.9.0"}}}],"version":"2.1.0"}
+// end of SIFIVE_CUSTOMIZATION
 // CHECK: 2 warnings and 6 errors generated.
