66d0aebd6fb171c9be72d94669ed482cad9c5505
Craig Topper <craig.topper@sifive.com>
2023-02-02 23:36:35 -0800

    [SiFive][Merge] Re-revert "RISCV: adjust relocation emission"
    This re-reverts commit 519a73111b40f0df1926879ab71e662b718bf109.
    
    This came back to life in a merge conflict resolution.
    
    It breaks the compact code model.

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFObjectWriter.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFObjectWriter.cpp
@@ -57,7 +57,7 @@ unsigned RISCVELFObjectWriter::getRelocType(MCContext &Ctx,
   if (IsPCRel) {
     switch (Kind) {
     default:
-      Ctx.reportError(Fixup.getLoc(), "Unsupported relocation type");
+      Ctx.reportError(Fixup.getLoc(), Twine("Unsupported relocation type1 ") + Twine(Kind));
       return ELF::R_RISCV_NONE;
     case FK_Data_4:
     case FK_PCRel_4:
@@ -107,7 +107,7 @@ unsigned RISCVELFObjectWriter::getRelocType(MCContext &Ctx,
 
   switch (Kind) {
   default:
-    Ctx.reportError(Fixup.getLoc(), "Unsupported relocation type");
+    Ctx.reportError(Fixup.getLoc(), "Unsupported relocation type2");
     return ELF::R_RISCV_NONE;
   case FK_Data_1:
     Ctx.reportError(Fixup.getLoc(), "1-byte data relocations not supported");
