71c981ce256e9d25e0d5ee22b8efca9ca2155ffa
Monk Chiang <monk.chiang@sifive.com>
2023-02-08 18:19:47 -0800

    Revert "[SiFive][Merge] Re-revert "RISCV: adjust relocation emission""
    This reverts commit 66d0aebd6fb171c9be72d94669ed482cad9c5505.

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFObjectWriter.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFObjectWriter.cpp
@@ -57,7 +57,7 @@ unsigned RISCVELFObjectWriter::getRelocType(MCContext &Ctx,
   if (IsPCRel) {
     switch (Kind) {
     default:
-      Ctx.reportError(Fixup.getLoc(), Twine("Unsupported relocation type1 ") + Twine(Kind));
+      Ctx.reportError(Fixup.getLoc(), "Unsupported relocation type");
       return ELF::R_RISCV_NONE;
     case FK_Data_4:
     case FK_PCRel_4:
@@ -107,7 +107,7 @@ unsigned RISCVELFObjectWriter::getRelocType(MCContext &Ctx,
 
   switch (Kind) {
   default:
-    Ctx.reportError(Fixup.getLoc(), "Unsupported relocation type2");
+    Ctx.reportError(Fixup.getLoc(), "Unsupported relocation type");
     return ELF::R_RISCV_NONE;
   case FK_Data_1:
     Ctx.reportError(Fixup.getLoc(), "1-byte data relocations not supported");
--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFStreamer.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVELFStreamer.cpp
@@ -192,18 +192,8 @@ void RISCVTargetELFStreamer::emitDirectiveVariantCC(MCSymbol &Symbol) {
   cast<MCSymbolELF>(Symbol).setOther(ELF::STO_RISCV_VARIANT_CC);
 }
 
-#if SIFIVE_CUSTOMIZATION
-// FIXME: This should be synced with upstream, but the upstream version breaks
-// the compact code model.
 bool RISCVELFStreamer::requiresFixups(MCContext &C, const MCExpr *Value,
                                       const MCExpr *&LHS, const MCExpr *&RHS) {
-  auto IsMetadataOrEHFrameSection = [](const MCSection &S) -> bool {
-    // Additionally check .apple_names/.apple_types. They are fixed-size and
-    // do not need fixups. llvm-dwarfdump --apple-names does not process
-    // R_RISCV_{ADD,SUB}32 in them.
-    return S.getKind().isMetadata() || S.getName() == ".eh_frame" ||
-           S.getName() == ".apple_names" || S.getName() == ".apple_types";
-  };
   const auto *MBE = dyn_cast<MCBinaryExpr>(Value);
   if (MBE == nullptr)
     return false;
@@ -217,22 +207,26 @@ bool RISCVELFStreamer::requiresFixups(MCContext &C, const MCExpr *Value,
   const auto &A = E.getSymA()->getSymbol();
   const auto &B = E.getSymB()->getSymbol();
 
-  LHS =
-      MCBinaryExpr::create(MCBinaryExpr::Add, MCSymbolRefExpr::create(&A, C),
-                           MCConstantExpr::create(E.getConstant(), C), C);
+  LHS = MCBinaryExpr::create(MCBinaryExpr::Add, MCSymbolRefExpr::create(&A, C),
+                             MCConstantExpr::create(E.getConstant(), C), C);
   RHS = E.getSymB();
 
-  // TODO: when available, R_RISCV_n_PCREL should be preferred.
+  // If either symbol is in a text section, we need to delay the relocation
+  // evaluation as relaxation may alter the size of the symbol.
+  //
+  // Unfortunately, we cannot identify if the symbol was built with relaxation
+  // as we do not track the state per symbol or section.  However, BFD will
+  // always emit the relocation and so we follow suit which avoids the need to
+  // track that information.
+  if (A.isInSection() && A.getSection().getKind().isText())
+    return true;
+  if (B.isInSection() && B.getSection().getKind().isText())
+    return true;
 
-  // Avoid pairwise relocations for symbolic difference in debug and .eh_frame
-  if (A.isInSection())
-    return !IsMetadataOrEHFrameSection(A.getSection());
-  if (B.isInSection())
-    return !IsMetadataOrEHFrameSection(B.getSection());
-  // as well as for absolute symbols.
-  return !A.getName().empty() || !B.getName().empty();
+  // Support cross-section symbolic differences ...
+  return A.isInSection() && B.isInSection() &&
+         A.getSection().getName() != B.getSection().getName();
 }
-#endif
 
 void RISCVELFStreamer::reset() {
   static_cast<RISCVTargetStreamer *>(getTargetStreamer())->reset();
