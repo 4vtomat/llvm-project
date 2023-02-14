8ac0d5d10ca688885af64372ae85749e1a690496
Han-Kuan Chen <hankuan.chen@sifive.com>
2023-02-13 21:45:37 -0800

    [SiFive][Recode] Pre-commit test.

---
+++ clang/test/Sema/sifive-recode-_Float16.c
@@ -0,0 +1,9 @@
+// RUN: %clang_cc1 %s -triple riscv64 -fsyntax-only -verify
+
+typedef _Float16 float16_t;
+typedef __attribute__((neon_vector_type(4))) float16_t float16x4_t;
+
+float16x4_t test(const float16_t *in_0)
+{
+  return __builtin_neon_vld1_v(in_0, 8); // expected-warning {{incompatible pointer types assigning to 'const __fp16 *' from 'const float16_t *' (aka 'const _Float16 *')}}
+}
