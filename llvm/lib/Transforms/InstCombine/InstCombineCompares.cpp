73501c895b3577b58fc19930b0dc29053e627767
Craig Topper <craig.topper@sifive.com>
2023-02-16 11:58:48 -0800

    [SiFive][InstCombine] Fix InstCombinerImpl::foldICmpMulConstant for nsw and nuw mul with unsigned compare.
    If we have both an nsw and nuw flag, we would see the nsw flag first and only handle signed comparisons.
    
    This patch ignores the nsw flag if the comparison isn't signed.
    
    Will look into adding a few more tests.
    
    SIFIVE: Cherry-picked from D143766.

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -2061,6 +2061,40 @@ Instruction *InstCombinerImpl::foldICmpMulConstant(ICmpInst &Cmp,
     }
   }
 
+#if SIFIVE_CUSTOMIZATION
+  // With a matching no-overflow guarantee, fold the constants:
+  // (X * MulC) < C --> X < (C / MulC)
+  // (X * MulC) > C --> X > (C / MulC)
+  // TODO: Assert that Pred is not equal to SGE, SLE, UGE, ULE?
+  Constant *NewC = nullptr;
+  if (Mul->hasNoSignedWrap() && ICmpInst::isSigned(Pred)) {
+    // MININT / -1 --> overflow.
+    if (C.isMinSignedValue() && MulC->isAllOnes())
+      return nullptr;
+    if (MulC->isNegative())
+      Pred = ICmpInst::getSwappedPredicate(Pred);
+
+    if (Pred == ICmpInst::ICMP_SLT || Pred == ICmpInst::ICMP_SGE) {
+      NewC = ConstantInt::get(
+          MulTy, APIntOps::RoundingSDiv(C, *MulC, APInt::Rounding::UP));
+    } else {
+      assert((Pred == ICmpInst::ICMP_SLE || Pred == ICmpInst::ICMP_SGT) &&
+             "Unexpected predicate");
+      NewC = ConstantInt::get(
+          MulTy, APIntOps::RoundingSDiv(C, *MulC, APInt::Rounding::DOWN));
+    }
+  } else if (Mul->hasNoUnsignedWrap() && ICmpInst::isUnsigned(Pred)) {
+    if (Pred == ICmpInst::ICMP_ULT || Pred == ICmpInst::ICMP_UGE) {
+      NewC = ConstantInt::get(
+          MulTy, APIntOps::RoundingUDiv(C, *MulC, APInt::Rounding::UP));
+    } else {
+      assert((Pred == ICmpInst::ICMP_ULE || Pred == ICmpInst::ICMP_UGT) &&
+             "Unexpected predicate");
+      NewC = ConstantInt::get(
+          MulTy, APIntOps::RoundingUDiv(C, *MulC, APInt::Rounding::DOWN));
+    }
+  }
+#else // SIFIVE_CUSTOMIZATION
   if (!Mul->hasNoSignedWrap() && !Mul->hasNoUnsignedWrap())
     return nullptr;
 
@@ -2091,6 +2125,7 @@ Instruction *InstCombinerImpl::foldICmpMulConstant(ICmpInst &Cmp,
       NewC = ConstantInt::get(
           MulTy, APIntOps::RoundingUDiv(C, *MulC, APInt::Rounding::DOWN));
   }
+#endif // SIFIVE_CUSTOMIZATION
 
   return NewC ? new ICmpInst(Pred, X, NewC) : nullptr;
 }
