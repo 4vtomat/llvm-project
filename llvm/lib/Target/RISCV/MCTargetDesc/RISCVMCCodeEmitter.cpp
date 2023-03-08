46f4219986d754df9806cd3eecd7adbda2ef60a5
Michael Berg <michael.berg@sifive.com>
2023-03-07 18:04:10 -0800

    [SiFive][Merge] Remove conflicts and keep downstream FeatureRelax functionality

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCCodeEmitter.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCCodeEmitter.cpp
@@ -192,13 +192,8 @@ void RISCVMCCodeEmitter::expandAddRegRel(const MCInst &MI, raw_ostream &OS,
   Fixups.push_back(MCFixup::create(
       0, Expr, MCFixupKind(FixupKind), MI.getLoc()));
 
-<<<<<<< HEAD
-  // Emit fixup_riscv_relax where the relax feature is enabled.
-  if (STI.getFeatureBits()[RISCV::FeatureRelax] && RelaxCandidate) {
-=======
   // Emit fixup_riscv_relax for tprel_add where the relax feature is enabled.
-  if (STI.hasFeature(RISCV::FeatureRelax)) {
->>>>>>> upstream/main
+  if (STI.getFeatureBits()[RISCV::FeatureRelax] && RelaxCandidate) {
     const MCConstantExpr *Dummy = MCConstantExpr::create(0, Ctx);
     Fixups.push_back(MCFixup::create(
         0, Dummy, MCFixupKind(RISCV::fixup_riscv_relax), MI.getLoc()));
