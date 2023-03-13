5501e69f1efee9bcb058c20243a8f50357b91c03
Monk Chiang <monk.chiang@sifive.com>
2023-03-12 23:58:23 -0700

    [SiFive][Merge] Remove conflicts for RISCVTargetMachine.cpp

--- llvm/lib/Target/RISCV/RISCVTargetMachine.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetMachine.cpp
@@ -426,18 +426,15 @@ void RISCVPassConfig::addPreEmitPass() {
 
 void RISCVPassConfig::addPreEmitPass2() {
   addPass(createRISCVExpandPseudoPass());
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
   addPass(createRISCVInsertNTLHInstsPass());
 #endif // SIFIVE_CUSTOMIZATION
-=======
 
   // Do the copy propagation after expanding pseudos because we may produce some
   // MVs when expanding.
   if (TM->getOptLevel() >= CodeGenOpt::Default && EnableRISCVCopyPropagation)
     addPass(createMachineCopyPropagationPass(true));
 
->>>>>>> upstream/main
   // Schedule the expansion of AMOs at the last possible moment, avoiding the
   // possibility for other passes to break the requirements for forward
   // progress in the LR/SC block.
