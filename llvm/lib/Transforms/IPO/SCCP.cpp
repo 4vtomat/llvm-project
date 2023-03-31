765ca74eac3224093df87339ef9c3daf18cf2e1b
Alexey Bataev <alexey.bataev@sifive.com>
2023-03-31 09:07:16 -0700

    Fix SCCP conflict

--- llvm/lib/Transforms/IPO/SCCP.cpp
+++ llvm/lib/Transforms/IPO/SCCP.cpp
@@ -280,12 +280,8 @@ static bool runIPSCCP(
 
     // If there is a known constant range for the return value, add !range
     // metadata to the function's call sites.
-#if SIFIVE_CUSTOMIZATION
-    // FIXME: The change was cherry-picked from upstrem.
-    // It should be removed for the next pull down merge.
-    if (F->getReturnType()->isIntegerTy() && ReturnValue.isConstantRange() &&
+    if (ReturnValue.isConstantRange() &&
         !ReturnValue.getConstantRange().isSingleElement()) {
-#endif // SIFIVE_CUSTOMIZATION
       // Do not add range metadata if the return value may include undef.
       if (ReturnValue.isConstantRangeIncludingUndef())
         continue;
