c79f68523df3b81d1098a51d1bf5a4cdbfff6428
Yeting Kuo <yeting.kuo@sifive.com>
2023-05-18 17:10:31 +0800

    [SiFive][Merge] Resolve conflict for llvm/include/llvm/IR/IntrinsicInst.h
    Resolve conflict from:
      commit 0b7f53efecb63659cbf4ebc2010a871e9b0121d3
      [VP] IR expansion for fabs/fsqrt/fma/fmadd
    
      Differential Revision: https://reviews.llvm.org/D149052

--- llvm/include/llvm/IR/IntrinsicInst.h
+++ llvm/include/llvm/IR/IntrinsicInst.h
@@ -604,17 +604,15 @@ public:
   // Equivalent non-predicated opcode
   static std::optional<unsigned> getFunctionalOpcodeForVP(Intrinsic::ID ID);
 
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
   // Return the vector prediction id of ID if ID has vp form. Otherwise, return
   // not_intrinsic.
   static Intrinsic::ID getVPIntrinsicID(Intrinsic::ID ID);
 #endif // SIFIVE_CUSTOMIZATION
-=======
+
   // Equivalent non-predicated constrained ID
   static std::optional<unsigned>
   getConstrainedIntrinsicIDForVP(Intrinsic::ID ID);
->>>>>>> upstream/main
 };
 
 /// This represents vector predication reduction intrinsics.
