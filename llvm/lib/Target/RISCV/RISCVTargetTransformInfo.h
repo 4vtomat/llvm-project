6f02cb3ffc3761addac0f8604dec0efe025c802a
Mel Chen <mel.chen@sifive.com>
2023-05-18 23:38:30 -0700

    [SiFive][RISCV] Enable reduction pattern SelectIVICmp and SelectIVFCmp.

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
@@ -390,6 +390,10 @@ public:
     case RecurKind::FMax:
     case RecurKind::SelectICmp:
     case RecurKind::SelectFCmp:
+#if SIFIVE_CUSTOMIZATION
+    case RecurKind::SelectIVICmp:
+    case RecurKind::SelectIVFCmp:
+#endif // SIFIVE_CUSTOMIZATION
     case RecurKind::FMulAdd:
       return true;
     default:
