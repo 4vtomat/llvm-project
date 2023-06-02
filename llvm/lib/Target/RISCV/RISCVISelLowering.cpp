163225c48b78149c6230448701504a1b0feab2cb
Yeting Kuo <yeting.kuo@sifive.com>
2023-06-02 04:44:16 -0700

    [SiFive][Merge] Avoid negateFMAOpcode assertion fail.
    To fix ICE for assertion fail when builing 433.milc for x280.

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -13315,6 +13315,12 @@ static SDValue combineVFMADD_VLWithVFNEG_VL(SDNode *N, SelectionDAG &DAG) {
   if (!NegA && !NegB && !NegC)
     return SDValue();
 
+#if SIFIVE_CUSTOMIZATION
+  // Avoid negateFMAOpcode assertion fail.
+  if (NegA == NegB && !NegC)
+    return SDValue();
+#endif // combineVFMADD_VLWithVFNEG_VL
+
   unsigned NewOpcode = negateFMAOpcode(N->getOpcode(), NegA != NegB, NegC);
   if (N->isTargetStrictFPOpcode())
     return DAG.getNode(NewOpcode, SDLoc(N), N->getVTList(),
