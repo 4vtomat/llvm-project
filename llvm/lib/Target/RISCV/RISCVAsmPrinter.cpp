85d07c7ceaeaf8a5ead04bb4527bf3c6805adb4b
4vtomat <brandon.wu@sifive.com>
2023-06-18 22:31:34 -0700

    [SiFive][RISCV] Add some target flags to switch statement in lowerSymbolOperand

--- llvm/lib/Target/RISCV/RISCVAsmPrinter.cpp
+++ llvm/lib/Target/RISCV/RISCVAsmPrinter.cpp
@@ -598,6 +598,35 @@ static MCOperand lowerSymbolOperand(const MachineOperand &MO, MCSymbol *Sym,
   case RISCVII::MO_TLS_GD_HI:
     Kind = RISCVMCExpr::VK_RISCV_TLS_GD_HI;
     break;
+#if SIFIVE_CUSTOMIZATION
+  case RISCVII::MO_TLS_GOT_GPREL_LO:
+    Kind = RISCVMCExpr::VK_RISCV_TLS_GOT_GPREL_LO;
+    break;
+  case RISCVII::MO_TLS_GOT_GPREL_HI:
+    Kind = RISCVMCExpr::VK_RISCV_TLS_GOT_GPREL_HI;
+    break;
+  case RISCVII::MO_TLS_GOT_GPREL_ADD:
+    Kind = RISCVMCExpr::VK_RISCV_TLS_GOT_GPREL_ADD;
+    break;
+  case RISCVII::MO_TLS_GD_GPREL_LO:
+    Kind = RISCVMCExpr::VK_RISCV_TLS_GD_GPREL_LO;
+    break;
+  case RISCVII::MO_TLS_GD_GPREL_HI:
+    Kind = RISCVMCExpr::VK_RISCV_TLS_GD_GPREL_HI;
+    break;
+  case RISCVII::MO_TLS_GD_GPREL_ADD:
+    Kind = RISCVMCExpr::VK_RISCV_TLS_GD_GPREL_ADD;
+    break;
+  case RISCVII::MO_GOT_GPREL_ADD:
+    Kind = RISCVMCExpr::VK_RISCV_GOT_GPREL_ADD;
+    break;
+  case RISCVII::MO_GOT_GPREL_LO:
+    Kind = RISCVMCExpr::VK_RISCV_GOT_GPREL_LO;
+    break;
+  case RISCVII::MO_GOT_GPREL_HI:
+    Kind = RISCVMCExpr::VK_RISCV_GOT_GPREL_HI;
+    break;
+#endif // SIFIVE_CUSTOMIZATION
   }
 
   const MCExpr *ME =
