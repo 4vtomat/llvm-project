2f651992e7e7cdc9a7e433f410b64f3cff3e0125
4vtomat <brandon.wu@sifive.com>
2023-06-19 04:36:11 -0700

    [SiFive] Add missing hasRoundModeOp in lowerRISCVVMachineInstrToMCInst

--- llvm/lib/Target/RISCV/RISCVAsmPrinter.cpp
+++ llvm/lib/Target/RISCV/RISCVAsmPrinter.cpp
@@ -713,6 +713,10 @@ static bool lowerRISCVVMachineInstrToMCInst(const MachineInstr *MI,
     --NumOps;
   if (RISCVII::hasVLOp(TSFlags))
     --NumOps;
+#ifdef SIFIVE_CUSTOMIZATION
+  if (RISCVII::hasRoundModeOp(TSFlags))
+    --NumOps;
+#endif // SIFIVE_CUSTOMIZATION
   if (RISCVII::hasSEWOp(TSFlags))
     --NumOps;
 
