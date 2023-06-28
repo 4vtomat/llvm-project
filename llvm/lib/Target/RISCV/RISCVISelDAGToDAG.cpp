9dcb037c5e8d98c542208c55fa51d4b1b0196e47
Nitin John Raj <nitin.raj@sifive.com>
2023-06-27 18:04:05 -0700

    [SiFive][Merge] llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp: Trivial merge

--- llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
+++ llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
@@ -3399,7 +3399,6 @@ bool RISCVDAGToDAGISel::doPeepholeMaskedRVV(SDNode *N) {
   return true;
 }
 
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
 bool RISCVDAGToDAGISel::doPeepholeLUIADDI(SDNode *N) {
   unsigned Opc = N->getMachineOpcode();
@@ -3446,13 +3445,6 @@ bool RISCVDAGToDAGISel::doPeepholeLUIADDI(SDNode *N) {
 }
 #endif // SIFIVE_CUSTOMIZATION
 
-static bool isImplicitDef(SDValue V) {
-  return V.isMachineOpcode() &&
-         V.getMachineOpcode() == TargetOpcode::IMPLICIT_DEF;
-}
-
-=======
->>>>>>> upstream-main
 // Try to fold away VMERGE_VVM instructions. We handle these cases:
 // -Masked TU VMERGE_VVM combined with an unmasked TA instruction instruction
 //  folds to a masked TU instruction. VMERGE_VVM must have have merge operand
