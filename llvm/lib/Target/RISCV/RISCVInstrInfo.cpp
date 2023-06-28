9edb7d111019579c2dbf9c71cba4713e8d3e56d5
Nitin John Raj <nitin.raj@sifive.com>
2023-06-27 18:13:33 -0700

    [SiFive][Merge] llvm/lib/Target/RISCV/RISCVInstrInfo.cpp: Merge in same function
    Upstream added code identical to our downstream SIFIVE_CUSTOMIZATION

--- llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
@@ -245,13 +245,7 @@ static bool isConvertibleToVMV_V_V(const RISCVSubtarget &STI,
       for (const MachineOperand &MO : MBBI->explicit_operands()) {
         if (!MO.isReg() || !MO.isDef())
           continue;
-<<<<<<< HEAD
-#if SIFIVE_CUSTOMIZATION
         if (!FoundDef && TRI->regsOverlap(MO.getReg(), SrcReg)) {
-#endif // SIFIVE_CUSTOMIZATION
-=======
-        if (!FoundDef && TRI->regsOverlap(MO.getReg(), SrcReg)) {
->>>>>>> upstream-main
           // We only permit the source of COPY has the same LMUL as the defined
           // operand.
           // There are cases we need to keep the whole register copy if the LMUL
