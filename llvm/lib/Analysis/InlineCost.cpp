6f7799c866b2213e24c795b4d3e909c6a3602bb5
Siddharth Swain <siddharth.shankar.swain@gmail.com>
2023-07-24 10:44:29 -0500

    [SiFive][Inliner] Modify Inlining heuristic to prevent regression in bzip2 (#1325)
    mainGTU() has the maximum effect on the performance ( major cause of regression ).
    mainGTU() is a relatively cold function with large number of 'if' conditional statements.
    
    Better inline decision is to not inline mainGTU().
    The current patch doesn't account for such penalty to the inline cost introduced by the inliner
    if it sees such functions ( mostly with large conditional branch statements ),
    therefore it inlines it by just seeing the no of parameters and code size.
    
    This patch aims to harden/ make strict the conditions for inlining in HDR
    ( this will basically isolate HDR improvement without affecting much the other benchmarks )
    by taking the inline cost to threshold distance
    as one of the factors to influence the inlining decision.
    
    The inline cost to threshold distance is added as an extra factor and if within permissble limit
    indicates to inline the function concerned. The inline to threshold distance limit is
    set to maximum of 150 ( by repeated experimentation with all the concerned benchmarks.)
    
    The test cases inline-param-size.ll is modified to take into account the case of bzip2
    ( not to inline mainGTU if the cost is too high even
    if the there are large number of function parameters and function size is small)
    and at the same time reatining the HDR inlining ( considering the number of parametrs
    and function size plus the cost threshold distance shld be within permissible limit).
    
    Co-authored-by: Siddharth Swain <siddharth.swain@sifive.com>

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -908,12 +908,12 @@ class InlineCostCallAnalyzer final : public CallAnalyzer {
     //Allow inlining when call overhead and function size wrt number of params
     //makes inlining beneficial and turn it off for amdgpu target.
     Module *M = CandidateCall.getFunction()->getParent();
-    std::string TargetTriple = M->getTargetTriple();
-    Triple T(TargetTriple);
-    if ((InlineParamSize)
-         && (T.getArchName() != "amdgcn")
-         && (NumParams >= 6)
-         && (FuncSize <= 500))
+    Triple T(M->getTargetTriple());
+    if (InlineParamSize &&
+    	!T.isAMDGCN() &&
+    	(NumParams >= 6) &&
+    	(FuncSize <= 500) &&
+    	(Cost - Threshold <= 150))
        Size = 1;
 #endif // SIFIVE_CUSTOMIZATION
 
@@ -994,12 +994,12 @@ class InlineCostCallAnalyzer final : public CallAnalyzer {
     //Allow inlining when call overhead and function size wrt number of params
     //makes inlining beneficial and turn it off for amdgpu target.
     Module *M = Caller->getParent();
-    std::string TargetTriple = M->getTargetTriple();
-    Triple T(TargetTriple);
-    if ((InlineParamSize)
-         && (T.getArchName() != "amdgcn")
-         && (NumParams >= 6)
-         && (FuncSize <= 500))
+    Triple T(M->getTargetTriple());
+    if (InlineParamSize &&
+    	!T.isAMDGCN() &&
+    	(NumParams >= 6) &&
+    	(FuncSize <= 500) &&
+    	(Cost - Threshold <= 150))
        return InlineResult::success();
 #endif // SIFIVE_CUSTOMIZATION
 
