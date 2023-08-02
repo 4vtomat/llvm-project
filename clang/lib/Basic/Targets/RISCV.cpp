68c4262bb4f0574dc0f3dcc91b278fb14153c9cf
Michael Maitland <michaeltmaitland@gmail.com>
2023-08-01 19:54:48 -0700

    [SiFive][Merge] Fix conflicts in clang/lib/basic/Targets/RISCV.cpp getTargetDefines

--- clang/lib/Basic/Targets/RISCV.cpp
+++ clang/lib/Basic/Targets/RISCV.cpp
@@ -198,19 +198,14 @@ void RISCVTargetInfo::getTargetDefines(const LangOptions &Opts,
 
   if (ISAInfo->hasExtension("zve32x")) {
     Builder.defineMacro("__riscv_vector");
-<<<<<<< HEAD
-    // Currently we support the v0.11 RISC-V V intrinsics.
-    Builder.defineMacro("__riscv_v_intrinsic", Twine(getVersionValue(0, 11)));
 #if SIFIVE_CUSTOMIZATION
     // Enable the inclusion of the compatible header by default.
     Builder.defineMacro("__rvv_0p10_compatible_intrinsics");
     // Enable the inclusion for compatibility support of v0.11 intrinsics
     Builder.defineMacro("__rvv_0p11_compatible_intrinsics");
 #endif
-=======
     // Currently we support the v0.12 RISC-V V intrinsics.
     Builder.defineMacro("__riscv_v_intrinsic", Twine(getVersionValue(0, 12)));
->>>>>>> upstream/main
   }
 
 #if SIFIVE_CUSTOMIZATION
