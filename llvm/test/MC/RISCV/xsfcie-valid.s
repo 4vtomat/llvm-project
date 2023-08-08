933ea3dac5def644f45d553fa566f480e9959407
Craig Topper <craig.topper@sifive.com>
2023-08-07 20:07:24 -0700

    [SiFive][Merge][RISCV] Partially revert D153499.
    Some of these SiFive specific CSRs have names that match a proposed
    standard extension. Our downstream compiler implements that names
    and addresses from the proposed extension. Our newest cores also
    use these addresses. The upstream code is based on old documentaton
    from SiFive.
    
    Remove the duplicate CSRs with the outdated addresses to match
    what's been in our downstream for a while.

--- llvm/test/MC/RISCV/xsfcie-valid.s
+++ llvm/test/MC/RISCV/xsfcie-valid.s
@@ -3,10 +3,10 @@
 # RUN:     | FileCheck -check-prefixes=CHECK-ENC,CHECK-INST %s
 # RUN: llvm-mc %s -triple=riscv64 -mattr=+xsfcie -riscv-no-aliases -show-encoding \
 # RUN:     | FileCheck -check-prefixes=CHECK-ENC,CHECK-INST %s
-# RUN: llvm-mc %s -triple=riscv32 -mattr=+xsfcie -riscv-no-aliases -show-encoding 2>&1 \
-# RUN:     | FileCheck -check-prefixes=CHECK-WARN %s
-# RUN: llvm-mc %s -triple=riscv64 -mattr=+xsfcie -riscv-no-aliases -show-encoding 2>&1 \
-# RUN:     | FileCheck -check-prefixes=CHECK-WARN %s
+# COM: llvm-mc %s -triple=riscv32 -mattr=+xsfcie -riscv-no-aliases -show-encoding 2>&1 \
+# COM:     | FileCheck -check-prefixes=CHECK-WARN %s
+# COM: llvm-mc %s -triple=riscv64 -mattr=+xsfcie -riscv-no-aliases -show-encoding 2>&1 \
+# COM:     | FileCheck -check-prefixes=CHECK-WARN %s
 # RUN: llvm-mc -filetype=obj -triple riscv32 -mattr=+xsfcie < %s \
 # RUN:     | llvm-objdump --mattr=+xsfcie -M no-aliases -d - \
 # RUN:     | FileCheck -check-prefix=CHECK-INST %s
@@ -15,8 +15,8 @@
 # RUN:     | FileCheck -check-prefix=CHECK-INST %s
 # RUN: llvm-mc %s -triple=riscv64 -mcpu=sifive-s76 -riscv-no-aliases -show-encoding \
 # RUN:     | FileCheck -check-prefixes=CHECK-ENC,CHECK-INST %s
-# RUN: llvm-mc %s -triple=riscv64 -mcpu=sifive-s76 -riscv-no-aliases -show-encoding 2>&1 \
-# RUN:     | FileCheck -check-prefixes=CHECK-WARN %s
+# COM: llvm-mc %s -triple=riscv64 -mcpu=sifive-s76 -riscv-no-aliases -show-encoding 2>&1 \
+# COM:     | FileCheck -check-prefixes=CHECK-WARN %s
 # RUN: llvm-mc -filetype=obj -triple riscv64 -mcpu=sifive-s76 < %s \
 # RUN:     | llvm-objdump --mcpu=sifive-s76 -M no-aliases -d - \
 # RUN:     | FileCheck -check-prefix=CHECK-INST %s
@@ -85,52 +85,52 @@ csrrs t2, 0x7C8, zero
 
 # mnscratch
 # name
-# CHECK-INST: csrrs t1, mnscratch, zero
-# CHECK-ENC: encoding: [0x73,0x23,0x00,0x35]
-# CHECK-WARN: warning: 'miselect' CSR is not available on the current subtarget. Instead 'mnscratch' CSR will be used.
+# COM-CHECK-INST: csrrs t1, mnscratch, zero
+# COM-CHECK-ENC: encoding: [0x73,0x23,0x00,0x35]
+# COM-CHECK-WARN: warning: 'miselect' CSR is not available on the current subtarget. Instead 'mnscratch' CSR will be used.
 # uimm12
-# CHECK-INST: csrrs t2, mnscratch, zero
-# CHECK-ENC: encoding: [0xf3,0x23,0x00,0x35]
+# COM-CHECK-INST: csrrs t2, mnscratch, zero
+# COM-CHECK-ENC: encoding: [0xf3,0x23,0x00,0x35]
 # name
-csrrs t1, mnscratch, zero
-csrrs t1, miselect, zero
+#csrrs t1, mnscratch, zero
+#csrrs t1, miselect, zero
 # uimm12
-csrrs t2, 0x350, zero
+#csrrs t2, 0x350, zero
 
 # mnepc
 # name
-# CHECK-INST: csrrs t1, mnepc, zero
-# CHECK-ENC: encoding: [0x73,0x23,0x10,0x35]
-# CHECK-WARN: warning: 'mireg' CSR is not available on the current subtarget. Instead 'mnepc' CSR will be used.
+# COM-CHECK-INST: csrrs t1, mnepc, zero
+# COM-CHECK-ENC: encoding: [0x73,0x23,0x10,0x35]
+# COM-CHECK-WARN: warning: 'mireg' CSR is not available on the current subtarget. Instead 'mnepc' CSR will be used.
 # uimm12
-# CHECK-INST: csrrs t2, mnepc, zero
-# CHECK-ENC: encoding: [0xf3,0x23,0x10,0x35]
+# COM-CHECK-INST: csrrs t2, mnepc, zero
+# COM-CHECK-ENC: encoding: [0xf3,0x23,0x10,0x35]
 # name
-csrrs t1, mnepc, zero
-csrrs t1, mireg, zero
+#csrrs t1, mnepc, zero
+#csrrs t1, mireg, zero
 # uimm12
-csrrs t2, 0x351, zero
+#csrrs t2, 0x351, zero
 
 # mncause
 # name
-# CHECK-INST: csrrs t1, mncause, zero
-# CHECK-ENC: encoding: [0x73,0x23,0x20,0x35]
+# COM-CHECK-INST: csrrs t1, mncause, zero
+# COM-CHECK-ENC: encoding: [0x73,0x23,0x20,0x35]
 # uimm12
-# CHECK-INST: csrrs t2, mncause, zero
-# CHECK-ENC: encoding: [0xf3,0x23,0x20,0x35]
+# COM-CHECK-INST: csrrs t2, mncause, zero
+# COM-CHECK-ENC: encoding: [0xf3,0x23,0x20,0x35]
 # name
-csrrs t1, mncause, zero
+#csrrs t1, mncause, zero
 # uimm12
-csrrs t2, 0x352, zero
+#csrrs t2, 0x352, zero
 
 # mnstatus
 # name
-# CHECK-INST: csrrs t1, mnstatus, zero
-# CHECK-ENC: encoding: [0x73,0x23,0x30,0x35]
+# COM-CHECK-INST: csrrs t1, mnstatus, zero
+# COM-CHECK-ENC: encoding: [0x73,0x23,0x30,0x35]
 # uimm12
-# CHECK-INST: csrrs t2, mnstatus, zero
-# CHECK-ENC: encoding: [0xf3,0x23,0x30,0x35]
+# COM-CHECK-INST: csrrs t2, mnstatus, zero
+# COM-CHECK-ENC: encoding: [0xf3,0x23,0x30,0x35]
 # name
-csrrs t1, mnstatus, zero
+#csrrs t1, mnstatus, zero
 # uimm12
-csrrs t2, 0x353, zero
+#csrrs t2, 0x353, zero
