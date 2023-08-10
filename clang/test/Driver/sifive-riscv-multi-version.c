54f156f6f35b8a7fc836468cd65f12af80d78fa9
Craig Topper <craig.topper@sifive.com>
2023-08-09 17:50:57 -0700

    [SiFive][Merge] Add 'not' to expect error command lines in sifive-riscv-multi-version.c
    Change vector crypto extension from 0.5 to 1.0 This was missed in
    a previous merge.

--- clang/test/Driver/sifive-riscv-multi-version.c
+++ clang/test/Driver/sifive-riscv-multi-version.c
@@ -1,12 +1,12 @@
 // RUN: %clang -target riscv32-unknown-elf -march=rv32ia2p1 -### %s -fsyntax-only 2>&1 | FileCheck %s
 // CHECK: "-target-feature" "+a"
 
-// RUN: %clang -target riscv32-unknown-elf -march=rv32ia2p2 -### %s -fsyntax-only 2>&1 \
+// RUN: not %clang -target riscv32-unknown-elf -march=rv32ia2p2 -### %s -fsyntax-only 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-UNSUPPORTED-VERSION %s
 // CHECK-UNSUPPORTED-VERSION: error: invalid arch name 'rv32ia2p2', unsupported version number 2.2 for
 // CHECK-UNSUPPORTED-VERSION: extension 'a' (this compiler supports 2.1)
 
-// RUN: %clang -target riscv32-unknown-elf -march=rv32g_zve64x_zvkg0p5 -menable-experimental-extensions -### %s \
+// RUN: %clang -target riscv32-unknown-elf -march=rv32g_zve64x_zvkg1p0 -menable-experimental-extensions -### %s \
 // RUN: -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-EXP-VERSION %s
 // RUN: %clang -target riscv32-unknown-elf -march=rv32g_zve64x_zvkg0p1 -menable-experimental-extensions -### %s \
 // RUN: -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-EXP-VERSION-1 %s
@@ -14,7 +14,7 @@
 // CHECK-EXP-VERSION: "-target-feature" "+experimental-zvkg"
 // CHECK-EXP-VERSION-1: "-target-feature" "+experimental-zvkg0p1"
 
-// RUN: %clang -target riscv32-unknown-elf -march=rv32gzba1p1 -### %s \
+// RUN: not %clang -target riscv32-unknown-elf -march=rv32gzba1p1 -### %s \
 // RUN: -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-EXP-UNSUPPORTED-VERSION %s
 //
 // CHECK-EXP-UNSUPPORTED-VERSION: error: invalid arch name 'rv32gzba1p1', unsupported version number 1.1
