2754150c7b3349023596fe3d4af08562435cc6ad
Piyou Chen <piyou.chen@sifive.com>
2023-08-14 00:39:47 -0700

    [SiFive][Merge] Fix conflicts in riscv_ntlh.h

--- clang/lib/Headers/riscv_ntlh.h
+++ clang/lib/Headers/riscv_ntlh.h
@@ -23,13 +23,6 @@ enum {
   __RISCV_NTLH_ALL
 };
 
-<<<<<<< HEAD
-#define __riscv_ntl_load(PTR, DOMAIN) __builtin_riscv_ntl_load((PTR), (DOMAIN))
-#define __riscv_ntl_store(PTR, VAL, DOMAIN)                                    \
-  __builtin_riscv_ntl_store((PTR), (VAL), (DOMAIN))
-
-=======
 #define __riscv_ntl_load __builtin_riscv_ntl_load
 #define __riscv_ntl_store __builtin_riscv_ntl_store
->>>>>>> upstream/main
 #endif
