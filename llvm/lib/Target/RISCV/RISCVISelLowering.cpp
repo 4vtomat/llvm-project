e041e631cbb81ff43952b1a6689f9c2173287a0a
Craig Topper <craig.topper@sifive.com>
2023-09-06 13:19:53 -0700

    [SiFive][RISCV] Remove old customization. NFC
    The referenced bug was fixed a different way upstream.

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -13273,12 +13273,6 @@ struct NodeExtensionHelper {
       break;
     }
     default:
-#if SIFIVE_CUSTOMIZATION
-      // We aren't going to modify this operand so it doesn't matter how many
-      // uses it has. This is a workaround for a larger issue described in
-      // https://github.com/llvm/llvm-project/issues/59345
-      EnforceOneUse = false;
-#endif // SIFIVE_CUSTOMIZATION
       break;
     }
   }
