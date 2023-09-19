4621cd146b9b8dff4bee03f7084e32c57699695c
Craig Topper <craig.topper@sifive.com>
2023-09-18 19:03:43 -0700

    [SiFive] Add REQUIRES amdgpu-registered-target or nvptx-registered-target to two OpenMP tests to fix CI.

--- clang/test/OpenMP/nvptx_throw_trap.cpp
+++ clang/test/OpenMP/nvptx_throw_trap.cpp
@@ -1,3 +1,5 @@
+// REQUIRES: nvptx-registered-target
+
 // RUN: %clang_cc1 -fopenmp -triple nvptx64 -fopenmp-is-target-device %s -emit-llvm -S -Wno-openmp-target-exception -o - | FileCheck -check-prefix=DEVICE %s
 // RUN: %clang_cc1 -fopenmp -triple x86_64-pc-linux-gnu -fopenmp-is-target-device -fcxx-exceptions %s -emit-llvm -S -Wno-openmp-target-exception -o - | FileCheck -check-prefix=HOST %s
 // DEVICE: trap;
