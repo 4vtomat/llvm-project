886c693fb9cf93de52cf050691259723f0aa958d
William Tsai <williamtsai1111@gmail.com>
2023-10-05 02:19:47 -0500

    [SiFive] Bumping LLVM_VERSION_PATCH to weekly 111 (#1496)
    We will be using LLVM_VERSION_PATCH to record which weekly build version
    the source is based on.

--- clang/test/Frontend/sarif-diagnostics.cpp
+++ clang/test/Frontend/sarif-diagnostics.cpp
@@ -65,6 +65,6 @@ void f1(t1 x, t1 y) {
 // CHECK: {"enabled":true,"level":"error","rank":50},"fullDescription":{"text":""},"id":"{{[0-9]+}}","name":""},{"defaultConfiguration":
 // CHECK: {"enabled":true,"level":"error","rank":50},"fullDescription":
 // SIFIVE_CUSTOMIZATION
-// CHECK: {"text":""},"id":"{{[0-9]+}}","name":""}],"version":"17.9.0"}}}],"version":"2.1.0"}
+// CHECK: {"text":""},"id":"{{[0-9]+}}","name":""}],"version":"17.9.{{[0-9]+}}"}}}],"version":"2.1.0"}
 // end of SIFIVE_CUSTOMIZATION
 // CHECK: 2 warnings and 6 errors generated.
