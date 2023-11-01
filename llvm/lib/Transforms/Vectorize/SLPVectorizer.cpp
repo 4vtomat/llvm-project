c9f56ca231816ff8964141c229d5133fa1a99488
Alexey Bataev <5361294+alexey-bataev@users.noreply.github.com>
2023-11-01 16:51:14 -0400

    [SIFIVE][SLP]Fix SCT-3151: strided operations for unsupported types. (#1539)
    Need to check before trying to emit strided instructions that the type
    is supported.

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -8492,7 +8492,17 @@ BoUpSLP::getEntryCost(const TreeEntry *E, ArrayRef<Value *> VectorizedVals,
     auto GetVectorCost = [&](InstructionCost CommonCost) {
       InstructionCost VecLdCost;
 #if SIFIVE_CUSTOMIZATION
-      bool IsReverse = isReverseOrder(E->ReorderIndices);
+      unsigned Sz = E->Scalars.size();
+      bool IsReverse =
+          isReverseOrder(E->ReorderIndices) &&
+          (E->ReuseShuffleIndices.empty() ||
+           !ShuffleVectorInst::isOneUseSingleSourceMask(E->ReuseShuffleIndices,
+                                                        Sz) ||
+           none_of(seq<unsigned>(0, E->ReuseShuffleIndices.size() / Sz),
+                   [&](unsigned Idx) {
+                     return ShuffleVectorInst::isReverseMask(
+                         ArrayRef(E->ReuseShuffleIndices).slice(Idx * Sz, Sz));
+                   }));
       if (E->State == TreeEntry::Vectorize || isRISCVStridedNode(E)) {
         Value *Ptr0 = cast<LoadInst>(VL.front())->getPointerOperand();
         Value *PtrN = cast<LoadInst>(VL.back())->getPointerOperand();
@@ -8501,12 +8511,10 @@ BoUpSLP::getEntryCost(const TreeEntry *E, ArrayRef<Value *> VectorizedVals,
         // Consecutive but reversed loads are just strided loads with the stride
         // -1.
         int Stride = Diff ? (*Diff / (static_cast<int>(VL.size()) - 1)) : 0;
+        Align CommonAlignment = computeCommonAlignment<LoadInst>(VL);
         if (enabledRISCVExtensions(*LI0->getModule(), *TTI) &&
-            (Stride != 1 || IsReverse)) {
-          Align CommonAlignment = LI0->getAlign();
-          for (Value *V : VL)
-            CommonAlignment =
-                std::min(CommonAlignment, cast<LoadInst>(V)->getAlign());
+            (Stride != 1 ||
+             (IsReverse && TTI->isLegalMaskedGather(VecTy, CommonAlignment)))) {
           VecLdCost = TTI->getGatherScatterOpCost(
               Instruction::Load, VecTy, Ptr0,
               /*VariableMask=*/false, CommonAlignment, CostKind, VL0);
@@ -8566,9 +8574,10 @@ BoUpSLP::getEntryCost(const TreeEntry *E, ArrayRef<Value *> VectorizedVals,
 #if SIFIVE_CUSTOMIZATION
       // Consecutive but reversed stores are just strided stores with the stride
       // -1.
+      Align CommonAlignment = computeCommonAlignment<StoreInst>(VL);
       if (isAllowedStridedStore(*BaseSI->getModule(), E->ReorderIndices,
-                                *TTI)) {
-        Align CommonAlignment = computeCommonAlignment<StoreInst>(VL);
+                                *TTI) &&
+          TTI->isLegalMaskedScatter(VecTy, CommonAlignment)) {
         // CommonCost can be ignored, no need to reverse before store.
         return TTI->getGatherScatterOpCost(
             Instruction::Store, VecTy, BaseSI->getPointerOperand(),
@@ -11248,7 +11257,17 @@ Value *BoUpSLP::vectorizeTree(TreeEntry *E) {
       Instruction *NewLI;
       Value *PO = LI->getPointerOperand();
 #if SIFIVE_CUSTOMIZATION
-      bool IsReverse = isReverseOrder(E->ReorderIndices);
+      unsigned Sz = E->Scalars.size();
+      bool IsReverse =
+          isReverseOrder(E->ReorderIndices) &&
+          (E->ReuseShuffleIndices.empty() ||
+           !ShuffleVectorInst::isOneUseSingleSourceMask(E->ReuseShuffleIndices,
+                                                        Sz) ||
+           none_of(seq<unsigned>(0, E->ReuseShuffleIndices.size() / Sz),
+                   [&](unsigned Idx) {
+                     return ShuffleVectorInst::isReverseMask(
+                         ArrayRef(E->ReuseShuffleIndices).slice(Idx * Sz, Sz));
+                   }));
       if (E->State == TreeEntry::Vectorize || isRISCVStridedNode(E)) {
         Value *Ptr0 = cast<LoadInst>(E->Scalars.front())->getPointerOperand();
         Value *PtrN = cast<LoadInst>(E->Scalars.back())->getPointerOperand();
@@ -11256,17 +11275,14 @@ Value *BoUpSLP::vectorizeTree(TreeEntry *E) {
             VL0->getType(), Ptr0, VL0->getType(), PtrN, *DL, *SE);
         int Stride =
             Diff ? (*Diff / (static_cast<int>(E->Scalars.size()) - 1)) : 0;
+        Align CommonAlignment = computeCommonAlignment<LoadInst>(E->Scalars);
         if (enabledRISCVExtensions(*LI->getModule(), *TTI) &&
-            (Stride != 1 || IsReverse)) {
+            (Stride != 1 ||
+             (IsReverse && TTI->isLegalMaskedGather(VecTy, CommonAlignment)))) {
           // Do not reorder strided loads.
           IgnoreReorder = IsReverse;
           Type *StrideTy = DL->getIndexType(PO->getType());
           Value *Ptr = IsReverse ? PtrN : Ptr0;
-          // Use the minimum alignment of the gathered loads.
-          Align CommonAlignment = LI->getAlign();
-          for (Value *V : E->Scalars)
-            CommonAlignment =
-                std::min(CommonAlignment, cast<LoadInst>(V)->getAlign());
           if (Stride != 0) {
             // Do not reorder reversed loads, just use -stride instead.
             if (IsReverse)
@@ -11374,7 +11390,9 @@ Value *BoUpSLP::vectorizeTree(TreeEntry *E) {
 #if SIFIVE_CUSTOMIZATION
       // Consecutive but reversed stores are just strided stores with the stride
       // -1.
-      if (isAllowedStridedStore(*SI->getModule(), E->ReorderIndices, *TTI)) {
+      Align CommonAlignment = computeCommonAlignment<StoreInst>(E->Scalars);
+      if (isAllowedStridedStore(*SI->getModule(), E->ReorderIndices, *TTI) &&
+          TTI->isLegalMaskedScatter(VecTy, CommonAlignment)) {
         SI = cast<StoreInst>(E->Scalars.back());
 
         Value *ScalarPtr = SI->getPointerOperand();
@@ -11382,11 +11400,6 @@ Value *BoUpSLP::vectorizeTree(TreeEntry *E) {
         Value *VecPtr = Builder.CreateBitCast(
             ScalarPtr, VecValue->getType()->getPointerTo(AS));
         Type *StrideTy = DL->getIndexType(VecPtr->getType());
-        // Use the minimum alignment of the gathered loads.
-        Align CommonAlignment = SI->getAlign();
-        for (Value *V : E->Scalars)
-          CommonAlignment =
-              std::min(CommonAlignment, cast<StoreInst>(V)->getAlign());
         auto *ST = Builder.CreateIntrinsic(
             Intrinsic::riscv_masked_strided_store,
             {VecTy, VecPtr->getType(), StrideTy},
