f3660159acd68b8d89a88e1a8391ddacb551d66c
Kito Cheng <kito.cheng@sifive.com>
2023-11-27 01:23:59 -0800

    [SiFive][RISCV] Fix instruction sequence for vector strict rounding operations
    The result of `fadd` will use as an input operand with mask undisturbed policy,
    so it must use mask undisturbed policy as well when compute the result.
    
    Fix SCT-3217.

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -3020,7 +3020,9 @@ lowerVectorStrictFTRUNC_FCEIL_FFLOOR_FROUND(SDValue Op, SelectionDAG &DAG,
   Chain = Unorder.getValue(1);
   Src = DAG.getNode(RISCVISD::STRICT_FADD_VL, DL,
                     DAG.getVTList(ContainerVT, MVT::Other),
-                    {Chain, Src, Src, DAG.getUNDEF(ContainerVT), Unorder, VL});
+#if SIFIVE_CUSTOMIZATION
+                    {Chain, Src, Src, Src, Unorder, VL});
+#endif // SIFIVE_CUSTOMIZATION
   Chain = Src.getValue(1);
 
   // We do the conversion on the absolute value and fix the sign at the end.
