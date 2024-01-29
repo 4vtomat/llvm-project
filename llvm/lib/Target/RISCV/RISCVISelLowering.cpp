7ae05da076ba37cd58600a551d2d91f914cc16bf
Craig Topper <craig.topper@sifive.com>
2024-01-28 20:46:02 -0800

    [SiFive][Merge] Cherry-pick 5cf9f2cd9888feea23a624c1de3cc37ce8ce8112 from upstream
    [RISCV] Fix M1 shuffle on wrong SrcVec in lowerShuffleViaVRegSplitting
    
    This fixes a miscompile from #79072 where we were taking the wrong SrcVec to do
    the M1 shuffle. E.g. if the SrcVecIdx was 2 and we had 2 VRegsPerSrc, we ended
    up taking it from V1 instead of V2.

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -4874,7 +4874,10 @@ static SDValue lowerShuffleViaVRegSplitting(ShuffleVectorSDNode *SVN,
     if (SrcVecIdx == -1)
       continue;
     unsigned ExtractIdx = (SrcVecIdx % VRegsPerSrc) * NumOpElts;
-    SDValue SrcVec = (unsigned)SrcVecIdx > VRegsPerSrc ? V2 : V1;
+#if SIFIVE_CUSTOMIZATION
+    // SIFIVE cherry-picked from upstream
+    SDValue SrcVec = (unsigned)SrcVecIdx >= VRegsPerSrc ? V2 : V1;
+#endif
     SDValue SubVec = DAG.getNode(ISD::EXTRACT_SUBVECTOR, DL, M1VT, SrcVec,
                                  DAG.getVectorIdxConstant(ExtractIdx, DL));
     SubVec = convertFromScalableVector(OneRegVT, SubVec, DAG, Subtarget);
