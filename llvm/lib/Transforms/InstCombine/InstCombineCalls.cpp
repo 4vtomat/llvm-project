4302cea0cabf2ff73a93a23a7e3e72514f79ca8b
Craig Topper <craig.topper@sifive.com>
2024-02-23 19:16:46 -0800

    Revert "[SiFive][InstCombine] In canEvaluateVPReversed, check that the mask of every operation is a splat."
    This reverts commit f1279bf8df60305449d944cb424b322366fb2b44.

--- llvm/lib/Transforms/InstCombine/InstCombineCalls.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCalls.cpp
@@ -1453,7 +1453,8 @@ static Instruction *foldBitOrderCrossLogicOp(Value *V,
 }
 
 #if SIFIVE_CUSTOMIZATION
-static bool canEvaluateVPReversed(Value *V, Value *VL, unsigned Depth = 5) {
+static bool canEvaluateVPReversed(Value *V, Value *Mask, Value *VL,
+                                  unsigned Depth = 5) {
   // Splats can be freely reversed.
   if (isSplatValue(V))
     return true;
@@ -1469,7 +1470,7 @@ static bool canEvaluateVPReversed(Value *V, Value *VL, unsigned Depth = 5) {
     return false;
 
   // Mask must be a splat and VL must match.
-  if (!isSplatValue(VPI->getMaskParam()) || VPI->getVectorLengthParam() != VL)
+  if (!isSplatValue(Mask) || VPI->getVectorLengthParam() != VL)
     return false;
 
   switch (VPI->getIntrinsicID()) {
@@ -1497,8 +1498,8 @@ static bool canEvaluateVPReversed(Value *V, Value *VL, unsigned Depth = 5) {
   case Intrinsic::vp_fadd:
   case Intrinsic::vp_fsub:
   case Intrinsic::vp_fmul: {
-    return canEvaluateVPReversed(VPI->getArgOperand(0), VL, Depth - 1) &&
-           canEvaluateVPReversed(VPI->getArgOperand(1), VL, Depth - 1);
+    return canEvaluateVPReversed(VPI->getArgOperand(0), Mask, VL, Depth - 1) &&
+           canEvaluateVPReversed(VPI->getArgOperand(1), Mask, VL, Depth - 1);
   }
   // FIXME: Add more unary, ternary, etc. operations.
   }
@@ -3348,7 +3349,7 @@ Instruction *InstCombinerImpl::visitCallInst(CallInst &CI) {
     Value *Vec = II->getArgOperand(0);
     Value *Mask = II->getArgOperand(1);
     Value *VL = II->getArgOperand(2);
-    if (isSplatValue(Mask) && canEvaluateVPReversed(Vec, VL)) {
+    if (canEvaluateVPReversed(Vec, Mask, VL)) {
       Value *V = evaluateVPReversed(Vec, *this);
       return replaceInstUsesWith(CI, V);
     }
