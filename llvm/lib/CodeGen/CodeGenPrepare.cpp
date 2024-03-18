dce4b3e09519b415a7fe2bc91643f851c7cfc299
Craig Topper <craig.topper@sifive.com>
2024-03-18 10:45:58 -0700

    [SiFive][CodeGenPrepare] Remove unneeded customization. NFC
    Upstream made a change that accomplishes the same thing in a different
    spot in this function.
    
    Fixes SCT-3575.

--- llvm/lib/CodeGen/CodeGenPrepare.cpp
+++ llvm/lib/CodeGen/CodeGenPrepare.cpp
@@ -2690,12 +2690,7 @@ bool CodeGenPrepare::dupRetToEnableTailCallOpts(BasicBlock *BB,
   ExtractValueInst *EVI = nullptr;
   BitCastInst *BCI = nullptr;
   Value *V = RetI->getReturnValue();
-#if SIFIVE_CUSTOMIZATION
-  // Treat undef similar to void.
-  if (V && !isa<UndefValue>(V)) {
-#else
   if (V) {
-#endif // SIFIVE_CUSTOMIZATION
     BCI = dyn_cast<BitCastInst>(V);
     if (BCI)
       V = BCI->getOperand(0);
