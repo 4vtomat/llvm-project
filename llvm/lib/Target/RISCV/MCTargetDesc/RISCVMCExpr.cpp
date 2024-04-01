fa8fc45ec43576dcd039dd53861f0e1b73b96d61
Yeting Kuo <yeting.kuo@sifive.com>
2024-04-01 14:32:20 +0800

    [SiFive][Merge] Resolve conflict for llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCExpr.cpp
    Resolve conflict from:
      commit 7cc9afdbdef3e8a5733112ed5c86697ed7c79e35
      [SiFive][Merge] Resolve conflicts and build errors.
    
      [SiFive][Merge] Resolve conflicts and build errors.

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCExpr.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCExpr.cpp
@@ -247,15 +247,12 @@ void RISCVMCExpr::fixELFSymbolsInTLSFixups(MCAssembler &Asm) const {
   case VK_RISCV_TLS_GOT_HI:
   case VK_RISCV_TLS_GD_HI:
   case VK_RISCV_TLSDESC_HI:
-<<<<<<< HEAD
   case VK_RISCV_TLSDESC_ADD_LO:
   case VK_RISCV_TLSDESC_LOAD_LO:
 #if SIFIVE_CUSTOMIZATION
   case VK_RISCV_TLS_GOT_GPREL_HI:
   case VK_RISCV_TLS_GD_GPREL_HI:
 #endif // SIFIVE_CUSTOMIZATION
-=======
->>>>>>> a9d1fead961440d415f931bc22c160dec88e03fd
     break;
   }
 
