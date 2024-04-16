79cb9d06a486e5b7dc59d97039da2d0344b086f4
Craig Topper <craig.topper@sifive.com>
2024-04-16 15:40:32 -0700

    [SiFive][RISCV] Re-separate unaligned scalar and vector memory features in the backend. (#88954)
    SIFIVE cherry-pick from 9067070d91e9d8cdd8509ffa56a076f08a3d7281
    
    This is largely a revert of commit
    e81796671890b59c110f8e41adc7ca26f8484d20.
    
    As #88029 shows, there exists hardware that only supports unaligned
    scalar.
    
    I'm leaving how this gets exposed to the clang interface to a future
    patch.

--- clang/lib/Basic/Targets/RISCV.cpp
+++ clang/lib/Basic/Targets/RISCV.cpp
@@ -402,7 +402,8 @@ bool RISCVTargetInfo::handleTargetFeatures(std::vector<std::string> &Features,
   if (ISAInfo->hasExtension("zfh") || ISAInfo->hasExtension("zhinx"))
     HasLegalHalfType = true;
 
-  FastUnalignedAccess = llvm::is_contained(Features, "+fast-unaligned-access");
+  FastUnalignedAccess = llvm::is_contained(Features, "+unaligned-scalar-mem") &&
+                        llvm::is_contained(Features, "+unaligned-vector-mem");
 #if SIFIVE_CUSTOMIZATION
   SlowVectorFP64 = llvm::is_contained(Features, "+slow-vector-fp64");
 #endif // SIFIVE_CUSTOMIZATION
