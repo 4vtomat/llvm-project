b95ac816591e3755059b828ddd15b4977c6d769f
Kolya Panchenko <kolya.panchenko@sifive.com>
2024-04-17 06:58:26 -0700

    [SiFive][LICM][Merge] Resolved LICM's conflicts

--- llvm/lib/Transforms/Scalar/LICM.cpp
+++ llvm/lib/Transforms/Scalar/LICM.cpp
@@ -3053,10 +3053,7 @@ static bool hoistMulAddAssociation(Instruction &I, Loop &L,
   if (maySpillForCandidate(&I, &L, LV, TTI, DT, IgnoreValues, Changes))
     return false;
 #endif
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/sifive-dev
   // Drop the poison flags for any adds we looked through.
   if (I.getType()->isIntOrIntVectorTy()) {
     for (auto *Add : Adds)
