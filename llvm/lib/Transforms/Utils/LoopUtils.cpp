90d0f0cb6cf11d378a33ccb0ee711cdfece83f61
Brandon Wu <brandon.wu@sifive.com>
2024-05-08 03:04:57 -0700

    [Merge] Resolve conflicts in llvm/lib/Transforms/Utils/LoopUtils.cpp

--- llvm/lib/Transforms/Utils/LoopUtils.cpp
+++ llvm/lib/Transforms/Utils/LoopUtils.cpp
@@ -1059,7 +1059,6 @@ CmpInst::Predicate llvm::getMinMaxReductionPredicate(RecurKind RK) {
   }
 }
 
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
 Value *llvm::createFindLastIVOp(IRBuilderBase &Builder, Value *Left,
                                 Value *Right) {
@@ -1067,8 +1066,6 @@ Value *llvm::createFindLastIVOp(IRBuilderBase &Builder, Value *Left,
 }
 #endif // SIFIVE_CUSTOMIZATION
 
-=======
->>>>>>> b329179
 Value *llvm::createAnyOfOp(IRBuilderBase &Builder, Value *StartVal,
                            RecurKind RK, Value *Left, Value *Right) {
   if (auto VTy = dyn_cast<VectorType>(Left->getType()))
@@ -1193,7 +1190,6 @@ Value *llvm::createAnyOfTargetReduction(IRBuilderBase &Builder, Value *Src,
   Value *Cmp =
       Builder.CreateCmp(CmpInst::ICMP_NE, Src, Right, "rdx.select.cmp");
 
-<<<<<<< HEAD
   // If any predicate is true it means that we want to select the new value.
   Cmp = Builder.CreateOrReduce(Cmp);
   return Builder.CreateSelect(Cmp, NewVal, InitVal, "rdx.select");
@@ -1257,11 +1253,6 @@ Value *llvm::createFindLastIVTargetReduction(IRBuilderBase &Builder,
              Desc.getRecurrenceKind()) &&
          "Unexpected reduction kind");
   return Builder.CreateIntMaxReduce(Src, RVL, true);
-=======
-  // If any predicate is true it means that we want to select the new value.
-  Cmp = Builder.CreateOrReduce(Cmp);
-  return Builder.CreateSelect(Cmp, NewVal, InitVal, "rdx.select");
->>>>>>> b329179
 }
 #endif // SIFIVE_CUSTOMIZATION
 
