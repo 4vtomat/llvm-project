ccc8ed81086ab3230104890d8e1726013f92c82b
Ming-Yi Lai <ming-yi.lai@mediatek.com>
2024-05-10 14:16:59 +0800

    [clang][RISCV] Add Zicfilp CFI scheme preprocessor macros
    These macros allow assembly files to know which CFI label to use when the target
    has Zicfilp enabled.

--- clang/lib/Basic/Targets/RISCV.cpp
+++ clang/lib/Basic/Targets/RISCV.cpp
@@ -255,6 +255,28 @@ void RISCVTargetInfo::getTargetDefines(const LangOptions &Opts,
     else
       Builder.defineMacro("__riscv_32e");
   }
+
+#if SIFIVE_CUSTOMIZATION
+// cherry-picked from upstream #109600
+  if (Opts.CFProtectionBranch) {
+    auto Scheme = Opts.getCFBranchLabelScheme();
+    if (Scheme == CFBranchLabelSchemeKind::Default)
+      Scheme = getDefaultCFBranchLabelScheme();
+
+    Builder.defineMacro("__riscv_landing_pad", "1");
+    switch (Scheme) {
+    case CFBranchLabelSchemeKind::Unlabeled:
+      Builder.defineMacro("__riscv_landing_pad_unlabeled", "1");
+      break;
+    case CFBranchLabelSchemeKind::FuncSig:
+      Builder.defineMacro("__riscv_landing_pad_func_sig", "1");
+      break;
+    case CFBranchLabelSchemeKind::Default:
+      llvm_unreachable("default cf-branch-label scheme should already be "
+                       "transformed to other scheme");
+    }
+  }
+#endif // SIFIVE_CUSTOMIZATION
 }
 
 static constexpr Builtin::Info BuiltinInfo[] = {
