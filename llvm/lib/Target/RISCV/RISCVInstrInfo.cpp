b594b8e0e34124d6fe567660c305b39ef70f0ea0
Craig Topper <craig.topper@sifive.com>
2024-06-27 18:55:40 -0700

    [SiFive][RISCV] Replace most of RISCVInstrInfo::expandLIsimm32 with a call to RISCVInstrInfo::movImm. NFC

--- llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
@@ -3967,56 +3967,13 @@ void RISCVInstrInfo::expandLIsimm32(MachineBasicBlock &MBB,
   int64_t Val = MI.getOperand(1).getImm();
   assert(isInt<32>(Val) && "Unexpected immediate");
 
-  RISCVMatInt::InstSeq Seq = RISCVMatInt::generateInstSeq(
-      Val, MBB.getParent()->getSubtarget());
-  assert(!Seq.empty());
-
-  Register SrcReg = RISCV::X0;
   Register DstReg = MI.getOperand(0).getReg();
   bool DstIsDead = MI.getOperand(0).isDead();
   bool Renamable = MI.getOperand(0).isRenamable();
-  bool SrcRenamable = false;
-  unsigned Num = 0;
 
-  for (RISCVMatInt::Inst &Inst : Seq) {
-    bool LastItem = ++Num == Seq.size();
-    switch (Inst.getOpndKind()) {
-    case RISCVMatInt::Imm:
-      BuildMI(MBB, MBBI, DL, get(Inst.getOpcode()))
-          .addReg(DstReg, RegState::Define |
-                              getDeadRegState(DstIsDead && LastItem) |
-                              getRenamableRegState(Renamable))
-          .addImm(Inst.getImm());
-      break;
-    case RISCVMatInt::RegX0:
-      BuildMI(MBB, MBBI, DL, get(Inst.getOpcode()))
-          .addReg(DstReg, RegState::Define |
-                              getDeadRegState(DstIsDead && LastItem) |
-                              getRenamableRegState(Renamable))
-          .addReg(SrcReg, RegState::Kill | getRenamableRegState(SrcRenamable))
-          .addReg(RISCV::X0);
-      break;
-    case RISCVMatInt::RegReg:
-      BuildMI(MBB, MBBI, DL, get(Inst.getOpcode()))
-          .addReg(DstReg, RegState::Define |
-                              getDeadRegState(DstIsDead && LastItem) |
-                              getRenamableRegState(Renamable))
-          .addReg(SrcReg, RegState::Kill | getRenamableRegState(SrcRenamable))
-          .addReg(SrcReg, RegState::Kill | getRenamableRegState(SrcRenamable));
-      break;
-    case RISCVMatInt::RegImm:
-      BuildMI(MBB, MBBI, DL, get(Inst.getOpcode()))
-          .addReg(DstReg, RegState::Define |
-                              getDeadRegState(DstIsDead && LastItem) |
-                              getRenamableRegState(Renamable))
-          .addReg(SrcReg, RegState::Kill | getRenamableRegState(SrcRenamable))
-          .addImm(Inst.getImm());
-      break;
-    }
-    // Only the first instruction has X0 as its source.
-    SrcReg = DstReg;
-    SrcRenamable = Renamable;
-  }
+  movImm(MBB, MBBI, DL, DstReg, Val, MachineInstr::NoFlags, Renamable,
+         DstIsDead);
+
   MI.eraseFromParent();
 }
 #endif // SIFIVE_CUSTOMIZATION
