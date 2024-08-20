f4b7f1a076611e01f970668f8131076fe1bed265
ShihPo Hung <shihpo.hung@sifive.com>
2024-08-20 02:56:56 -0700

    [SiFive][Merge] Resolve conflict in SLPVectorizer.cpp

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -10319,13 +10319,7 @@ bool BoUpSLP::isFullyVectorizableTinyTree(bool ForReduction) const {
   // We only handle trees of heights 1 and 2.
   if (VectorizableTree.size() == 1 &&
       (VectorizableTree[0]->State == TreeEntry::Vectorize ||
-<<<<<<< HEAD
-#if SIFIVE_CUSTOMIZATION
        VectorizableTree[0]->State == TreeEntry::StridedVectorize ||
-#endif // SIFIVE_CUSTOMIZATION
-=======
-       VectorizableTree[0]->State == TreeEntry::StridedVectorize ||
->>>>>>> ddda37a
        (ForReduction &&
         AreVectorizableGathers(VectorizableTree[0].get(),
                                VectorizableTree[0]->Scalars.size()) &&
