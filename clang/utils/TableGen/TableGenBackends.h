249ebc89c7d3092723618029172125fbe114d192
Michael Berg <michael.berg@sifive.com>
2024-09-19 17:12:54 -0700

    [SiFive][Merge] fixup conflicts for clang/utils/TableGen/TableGenBackends.h

--- clang/utils/TableGen/TableGenBackends.h
+++ clang/utils/TableGen/TableGenBackends.h
@@ -115,27 +115,16 @@ void EmitClangSyntaxNodeList(const llvm::RecordKeeper &Records,
 void EmitClangSyntaxNodeClasses(const llvm::RecordKeeper &Records,
                                 llvm::raw_ostream &OS);
 
-<<<<<<< HEAD
-void EmitNeon(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
-#if SIFIVE_CUSTOMIZATION
-void EmitRecodeNeon(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
-#endif
-void EmitFP16(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
-#if SIFIVE_CUSTOMIZATION
-void EmitRecodeFP16(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
-#endif
-void EmitBF16(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
-void EmitNeonSema(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
-void EmitVectorTypes(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
-void EmitNeonTest(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
-=======
 void EmitNeon(const llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
+#if SIFIVE_CUSTOMIZATION
+void EmitRecodeNeon(const llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
+void EmitRecodeFP16(const llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
+#endif
 void EmitFP16(const llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
 void EmitBF16(const llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
 void EmitNeonSema(const llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
 void EmitVectorTypes(const llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
 void EmitNeonTest(const llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
->>>>>>> c970e96
 
 void EmitImmCheckTypes(const llvm::RecordKeeper &Records,
                        llvm::raw_ostream &OS);
