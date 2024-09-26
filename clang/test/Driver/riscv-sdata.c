6ac0f5a83d9c6e95497601740cdefba15a088904
Craig Topper <craig.topper@sifive.com>
2024-09-25 22:53:30 -0700

    [SiFive][RISCV] Re-enable small data limit by default for baremetal. (#2219)
    Attempt to better handle the PIC and PIE flags than the original
    upstream code.
    
    Fixes SCT-4132

--- clang/test/Driver/riscv-sdata.c
+++ clang/test/Driver/riscv-sdata.c
@@ -1,4 +1,4 @@
-// RUN: %clang -### -S --target=riscv64 %s 2>&1 | FileCheck %s
+// RUN: %clang -### -S --target=riscv64-linux-gnu %s 2>&1 | FileCheck %s
 // RUN: %clang -### -S --target=riscv64 -msmall-data-limit=8 %s 2>&1 | FileCheck %s --check-prefix=EIGHT
 
 // CHECK-NOT: "-msmall-data-limit"
