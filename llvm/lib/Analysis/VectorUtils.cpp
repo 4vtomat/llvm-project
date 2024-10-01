7b71f689e0578cdf6dae7015d885dc9d1fe95ac3
Craig Topper <craig.topper@sifive.com>
2024-10-01 13:29:10 -0700

    [SiFive][Merge] Resolve conflict in VectorUtils.cpp.

--- llvm/lib/Analysis/VectorUtils.cpp
+++ llvm/lib/Analysis/VectorUtils.cpp
@@ -188,15 +188,12 @@ bool llvm::isVectorIntrinsicWithOverloadTypeAtArg(Intrinsic::ID ID,
   case Intrinsic::fptoui_sat:
   case Intrinsic::lrint:
   case Intrinsic::llrint:
-<<<<<<< HEAD
   case Intrinsic::ucmp:
   case Intrinsic::scmp:
-=======
 #if SIFIVE_CUSTOMIZATION
   case Intrinsic::vp_lrint:
   case Intrinsic::vp_llrint:
 #endif // SIFIVE_CUSTOMIZATION
->>>>>>> origin/sifive-dev
     return OpdIdx == -1 || OpdIdx == 0;
   case Intrinsic::is_fpclass:
     return OpdIdx == 0;
