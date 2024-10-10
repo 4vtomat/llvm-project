00af762f94cee240c9e87b1340baa3ebd31bd65a
Kito Cheng <kito.cheng@sifive.com>
2024-10-10 11:36:21 +0800

    [SiFive][Merge] Trivial merge for VPlanTestBase.h

--- llvm/unittests/Transforms/Vectorize/VPlanTestBase.h
+++ llvm/unittests/Transforms/Vectorize/VPlanTestBase.h
@@ -67,19 +67,13 @@ protected:
     assert(!verifyFunction(F) && "input function must be valid");
     doAnalysis(F);
 
-<<<<<<< HEAD
-    auto Plan = VPlan::createInitialVPlan(
-        SE->getBackedgeTakenCount(LI->getLoopFor(LoopHeader)), *SE, true, false,
-        /*IsUncountable=*/false, // SIFIVE
-        LI->getLoopFor(LoopHeader));
-    VPlanHCFGBuilder HCFGBuilder(LI->getLoopFor(LoopHeader), LI.get(), *Plan);
-=======
     Loop *L = LI->getLoopFor(LoopHeader);
     PredicatedScalarEvolution PSE(*SE, *L);
     auto Plan = VPlan::createInitialVPlan(IntegerType::get(*Ctx, 64), PSE, true,
-                                          false, L);
+                                          false,
+                                          /*IsUncountable=*/false, // SIFIVE
+                                          L);
     VPlanHCFGBuilder HCFGBuilder(L, LI.get(), *Plan);
->>>>>>> d8a656ffaf735ed689856daa5dc13a9274358072
     HCFGBuilder.buildHierarchicalCFG();
     return Plan;
   }
@@ -90,19 +84,13 @@ protected:
     assert(!verifyFunction(F) && "input function must be valid");
     doAnalysis(F);
 
-<<<<<<< HEAD
-    auto Plan = VPlan::createInitialVPlan(
-        SE->getBackedgeTakenCount(LI->getLoopFor(LoopHeader)), *SE, true, false,
-        /*IsUncountable=*/false, // SIFIVE
-        LI->getLoopFor(LoopHeader));
-    VPlanHCFGBuilder HCFGBuilder(LI->getLoopFor(LoopHeader), LI.get(), *Plan);
-=======
     Loop *L = LI->getLoopFor(LoopHeader);
     PredicatedScalarEvolution PSE(*SE, *L);
     auto Plan = VPlan::createInitialVPlan(IntegerType::get(*Ctx, 64), PSE, true,
-                                          false, L);
+                                          false,
+                                          /*IsUncountable=*/false, // SIFIVE
+                                          L);
     VPlanHCFGBuilder HCFGBuilder(L, LI.get(), *Plan);
->>>>>>> d8a656ffaf735ed689856daa5dc13a9274358072
     HCFGBuilder.buildPlainCFG();
     return Plan;
   }
