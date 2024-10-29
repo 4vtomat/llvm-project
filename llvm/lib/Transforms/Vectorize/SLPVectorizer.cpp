01969fe9a260a6eab45326b3cbb9ac60cafb1e73
Alexey Bataev <alexey.bataev@sifive.com>
2024-10-29 06:37:14 -0700

    [SiFIve][Merge]Backport b65b2b4ab60763515694c740935989f908a03312 to fix crashes in SLP

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -9583,13 +9583,25 @@ class BoUpSLP::ShuffleCostEstimator : public BaseShuffleAnalysis {
             ::getShuffleCost(TTI, *RegShuffleKind,
                              getWidenedType(ScalarTy, EltsPerVector), SubMask);
       }
+#if SIFIVE_CUSTOMIZATION
+      const unsigned BaseVF = getFullVectorNumberOfElements(
+          *R.TTI, VL.front()->getType(), alignTo(NumElts, EltsPerVector));
+#endif // SIFIVE_CUSTOMIZATION
       for (unsigned Idx : Indices) {
+#if SIFIVE_CUSTOMIZATION
+        assert((Idx + EltsPerVector) <= BaseVF &&
+               "SK_ExtractSubvector index out of range");
+        Cost += ::getShuffleCost(TTI, TTI::SK_ExtractSubvector,
+                                 getWidenedType(ScalarTy, BaseVF), {}, CostKind,
+                                 Idx, getWidenedType(ScalarTy, EltsPerVector));
+#else // SIFIVE_CUSTOMIZATION
         assert((Idx + EltsPerVector) <= alignTo(NumElts, EltsPerVector) &&
                "SK_ExtractSubvector index out of range");
         Cost += ::getShuffleCost(
             TTI, TTI::SK_ExtractSubvector,
             getWidenedType(ScalarTy, alignTo(NumElts, EltsPerVector)), {},
             CostKind, Idx, getWidenedType(ScalarTy, EltsPerVector));
+#endif // SIFIVE_CUSTOMIZATION
       }
       // Second attempt to check, if just a permute is better estimated than
       // subvector extract.
