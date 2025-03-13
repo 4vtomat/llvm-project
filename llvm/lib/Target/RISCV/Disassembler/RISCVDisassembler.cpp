71d751c35deebf3a47b3b6e99e8db37725dc290e
Craig Topper <craig.topper@sifive.com>
2025-03-12 18:55:32 -0700

    [SiFive][RISCV] Remove duplicate TRY_TO_DECODE_FEATURE lines. NFC (#2437)
    We have these outside of SIFIVE_CUSTOMIZATION too

--- llvm/lib/Target/RISCV/Disassembler/RISCVDisassembler.cpp
+++ llvm/lib/Target/RISCV/Disassembler/RISCVDisassembler.cpp
@@ -701,18 +701,6 @@ DecodeStatus RISCVDisassembler::getInstruction32(MCInst &MI, uint64_t &Size,
   TRY_TO_DECODE_FEATURE(RISCV::FeatureVendorXSfvqdotq,
                         DecoderTableXSfvqdotq32,
                         "SiFive vqdotq custom opcode table");
-  TRY_TO_DECODE_FEATURE(RISCV::FeatureVendorXSfvqmaccdod,
-                        DecoderTableXSfvqmaccdod32,
-                        "SiFive vqmaccdod custom opcode table");
-  TRY_TO_DECODE_FEATURE(RISCV::FeatureVendorXSfvqmaccqoq,
-                        DecoderTableXSfvqmaccqoq32,
-                        "SiFive vqmaccqoq custom opcode table");
-  TRY_TO_DECODE_FEATURE(RISCV::FeatureVendorXSfvfwmaccqqq,
-                        DecoderTableXSfvfwmaccqqq32,
-                        "SiFive vfwmaccqqq custom opcode table");
-  TRY_TO_DECODE_FEATURE(RISCV::FeatureVendorXSfvfnrclipxfqf,
-                        DecoderTableXSfvfnrclipxfqf32,
-                        "SiFive vfnrclipxfqf custom opcode table");
   TRY_TO_DECODE_FEATURE(RISCV::FeatureVendorXSfmmbase,
                         DecoderTableXSfmmbase32,
                         "SiFive XSfmmbase extension custom opcode table");
