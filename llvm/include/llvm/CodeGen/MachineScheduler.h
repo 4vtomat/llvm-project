a842f5c0bbfa1c77944718eef41e4c9913741944
Piyou Chen <piyou.chen@sifive.com>
2025-03-18 09:15:18 +0800

    [SiFive][RISCV][NFC] drop !Pending.empty() in bumpCycleUntilReleaseSUFromPending (#2450)
    Due to `llvm::find(Pending, SU) != Pending.end()` will catch empty
    situation.

--- llvm/include/llvm/CodeGen/MachineScheduler.h
+++ llvm/include/llvm/CodeGen/MachineScheduler.h
@@ -1071,7 +1071,7 @@ public:
 
 #ifdef SIFIVE_CUSTOMIZATION
   void bumpCycleUntilReleaseSUFromPending(SUnit *SU, unsigned ReadyListLimit) {
-    while (Available.size() < ReadyListLimit && !Pending.empty() &&
+    while (Available.size() < ReadyListLimit &&
            llvm::find(Pending, SU) != Pending.end()) {
       bumpCycle(CurrCycle + 1);
       releasePending();
