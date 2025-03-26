6f6e841f18bad3cace8cf9ad82983d2e839b9fc3
Piyou Chen <piyou.chen@sifive.com>
2025-03-26 07:33:13 -0700

    [SiFive][Merge] resolve clang/lib/CodeGen/CodeGenTBAA.cpp

--- clang/lib/CodeGen/CodeGenTBAA.cpp
+++ clang/lib/CodeGen/CodeGenTBAA.cpp
@@ -278,10 +278,7 @@ llvm::MDNode *CodeGenTBAA::getTypeInfoHelper(const Type *Ty) {
     do {
       PtrDepth++;
       Ty = Ty->getPointeeType()->getBaseElementTypeUnsafe();
-<<<<<<< HEAD
-    } while (Ty->isPointerType() || Ty->isReferenceType()); // SIFIVE
-=======
-    } while (Ty->isPointerType());
+    } while (Ty->isPointerType() || Ty->isReferenceType());
 
     // While there are no special rules in the standards regarding void pointers
     // and strict aliasing, emitting distinct tags for void pointers break some
@@ -290,7 +287,6 @@ llvm::MDNode *CodeGenTBAA::getTypeInfoHelper(const Type *Ty) {
     if (Ty->isVoidType())
       return AnyPtr;
 
->>>>>>> c06d0ff
     assert(!isa<VariableArrayType>(Ty));
     // When the underlying type is a builtin type, we compute the pointee type
     // string recursively, which is implicitly more forgiving than the standards
