62b1a22fb339c804c14ee64d35da8448c46b3805
Brandon Wu <songwu0813@gmail.com>
2025-04-16 13:26:56 +0800

    [RISCV] Change scalar operand of vqdot_vx to unsigned (#2499)
    Scalar operand of vqdot_vx contains 4 i8, it's not a single i32.

--- clang/test/CodeGen/RISCV/rvv-intrinsics-sifive/policy/overloaded/sf_vqdot_vx.c
+++ clang/test/CodeGen/RISCV/rvv-intrinsics-sifive/policy/overloaded/sf_vqdot_vx.c
@@ -13,7 +13,7 @@
 // CHECK-RV64-NEXT:    ret <vscale x 1 x i32> [[TMP0]]
 //
 vint32mf2_t test_sf_vqdot_vx_i32mf2_tu(vint32mf2_t vd, vint8mf2_t vs2,
-                                       int32_t rs1, size_t vl) {
+                                       uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_tu(vd, vs2, rs1, vl);
 }
 
@@ -24,7 +24,7 @@ vint32mf2_t test_sf_vqdot_vx_i32mf2_tu(vint32mf2_t vd, vint8mf2_t vs2,
 // CHECK-RV64-NEXT:    ret <vscale x 2 x i32> [[TMP0]]
 //
 vint32m1_t test_sf_vqdot_vx_i32m1_tu(vint32m1_t vd, vint8m1_t vs2,
-                                     int32_t rs1, size_t vl) {
+                                     uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_tu(vd, vs2, rs1, vl);
 }
 
@@ -35,7 +35,7 @@ vint32m1_t test_sf_vqdot_vx_i32m1_tu(vint32m1_t vd, vint8m1_t vs2,
 // CHECK-RV64-NEXT:    ret <vscale x 4 x i32> [[TMP0]]
 //
 vint32m2_t test_sf_vqdot_vx_i32m2_tu(vint32m2_t vd, vint8m2_t vs2,
-                                     int32_t rs1, size_t vl) {
+                                     uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_tu(vd, vs2, rs1, vl);
 }
 
@@ -46,7 +46,7 @@ vint32m2_t test_sf_vqdot_vx_i32m2_tu(vint32m2_t vd, vint8m2_t vs2,
 // CHECK-RV64-NEXT:    ret <vscale x 8 x i32> [[TMP0]]
 //
 vint32m4_t test_sf_vqdot_vx_i32m4_tu(vint32m4_t vd, vint8m4_t vs2,
-                                     int32_t rs1, size_t vl) {
+                                     uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_tu(vd, vs2, rs1, vl);
 }
 
@@ -57,7 +57,7 @@ vint32m4_t test_sf_vqdot_vx_i32m4_tu(vint32m4_t vd, vint8m4_t vs2,
 // CHECK-RV64-NEXT:    ret <vscale x 16 x i32> [[TMP0]]
 //
 vint32m8_t test_sf_vqdot_vx_i32m8_tu(vint32m8_t vd, vint8m8_t vs2,
-                                     int32_t rs1, size_t vl) {
+                                     uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_tu(vd, vs2, rs1, vl);
 }
 
@@ -68,7 +68,7 @@ vint32m8_t test_sf_vqdot_vx_i32m8_tu(vint32m8_t vd, vint8m8_t vs2,
 // CHECK-RV64-NEXT:    ret <vscale x 1 x i32> [[TMP0]]
 //
 vint32mf2_t test_sf_vqdot_vx_i32mf2_tum(vbool64_t vm, vint32mf2_t vd,
-                                        vint8mf2_t vs2, int32_t rs1,
+                                        vint8mf2_t vs2, uint32_t rs1,
                                         size_t vl) {
   return __riscv_sf_vqdot_tum(vm, vd, vs2, rs1, vl);
 }
@@ -80,7 +80,7 @@ vint32mf2_t test_sf_vqdot_vx_i32mf2_tum(vbool64_t vm, vint32mf2_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 2 x i32> [[TMP0]]
 //
 vint32m1_t test_sf_vqdot_vx_i32m1_tum(vbool32_t vm, vint32m1_t vd,
-                                      vint8m1_t vs2, int32_t rs1,
+                                      vint8m1_t vs2, uint32_t rs1,
                                       size_t vl) {
   return __riscv_sf_vqdot_tum(vm, vd, vs2, rs1, vl);
 }
@@ -92,7 +92,7 @@ vint32m1_t test_sf_vqdot_vx_i32m1_tum(vbool32_t vm, vint32m1_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 4 x i32> [[TMP0]]
 //
 vint32m2_t test_sf_vqdot_vx_i32m2_tum(vbool16_t vm, vint32m2_t vd,
-                                      vint8m2_t vs2, int32_t rs1,
+                                      vint8m2_t vs2, uint32_t rs1,
                                       size_t vl) {
   return __riscv_sf_vqdot_tum(vm, vd, vs2, rs1, vl);
 }
@@ -104,7 +104,7 @@ vint32m2_t test_sf_vqdot_vx_i32m2_tum(vbool16_t vm, vint32m2_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 8 x i32> [[TMP0]]
 //
 vint32m4_t test_sf_vqdot_vx_i32m4_tum(vbool8_t vm, vint32m4_t vd,
-                                      vint8m4_t vs2, int32_t rs1,
+                                      vint8m4_t vs2, uint32_t rs1,
                                       size_t vl) {
   return __riscv_sf_vqdot_tum(vm, vd, vs2, rs1, vl);
 }
@@ -116,7 +116,7 @@ vint32m4_t test_sf_vqdot_vx_i32m4_tum(vbool8_t vm, vint32m4_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 16 x i32> [[TMP0]]
 //
 vint32m8_t test_sf_vqdot_vx_i32m8_tum(vbool4_t vm, vint32m8_t vd,
-                                      vint8m8_t vs2, int32_t rs1,
+                                      vint8m8_t vs2, uint32_t rs1,
                                       size_t vl) {
   return __riscv_sf_vqdot_tum(vm, vd, vs2, rs1, vl);
 }
@@ -128,7 +128,7 @@ vint32m8_t test_sf_vqdot_vx_i32m8_tum(vbool4_t vm, vint32m8_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 1 x i32> [[TMP0]]
 //
 vint32mf2_t test_sf_vqdot_vx_i32mf2_tumu(vbool64_t vm, vint32mf2_t vd,
-                                         vint8mf2_t vs2, int32_t rs1,
+                                         vint8mf2_t vs2, uint32_t rs1,
                                          size_t vl) {
   return __riscv_sf_vqdot_tumu(vm, vd, vs2, rs1, vl);
 }
@@ -140,7 +140,7 @@ vint32mf2_t test_sf_vqdot_vx_i32mf2_tumu(vbool64_t vm, vint32mf2_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 2 x i32> [[TMP0]]
 //
 vint32m1_t test_sf_vqdot_vx_i32m1_tumu(vbool32_t vm, vint32m1_t vd,
-                                       vint8m1_t vs2, int32_t rs1,
+                                       vint8m1_t vs2, uint32_t rs1,
                                        size_t vl) {
   return __riscv_sf_vqdot_tumu(vm, vd, vs2, rs1, vl);
 }
@@ -152,7 +152,7 @@ vint32m1_t test_sf_vqdot_vx_i32m1_tumu(vbool32_t vm, vint32m1_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 4 x i32> [[TMP0]]
 //
 vint32m2_t test_sf_vqdot_vx_i32m2_tumu(vbool16_t vm, vint32m2_t vd,
-                                       vint8m2_t vs2, int32_t rs1,
+                                       vint8m2_t vs2, uint32_t rs1,
                                        size_t vl) {
   return __riscv_sf_vqdot_tumu(vm, vd, vs2, rs1, vl);
 }
@@ -164,7 +164,7 @@ vint32m2_t test_sf_vqdot_vx_i32m2_tumu(vbool16_t vm, vint32m2_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 8 x i32> [[TMP0]]
 //
 vint32m4_t test_sf_vqdot_vx_i32m4_tumu(vbool8_t vm, vint32m4_t vd,
-                                       vint8m4_t vs2, int32_t rs1,
+                                       vint8m4_t vs2, uint32_t rs1,
                                        size_t vl) {
   return __riscv_sf_vqdot_tumu(vm, vd, vs2, rs1, vl);
 }
@@ -176,7 +176,7 @@ vint32m4_t test_sf_vqdot_vx_i32m4_tumu(vbool8_t vm, vint32m4_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 16 x i32> [[TMP0]]
 //
 vint32m8_t test_sf_vqdot_vx_i32m8_tumu(vbool4_t vm, vint32m8_t vd,
-                                       vint8m8_t vs2, int32_t rs1,
+                                       vint8m8_t vs2, uint32_t rs1,
                                        size_t vl) {
   return __riscv_sf_vqdot_tumu(vm, vd, vs2, rs1, vl);
 }
@@ -188,7 +188,7 @@ vint32m8_t test_sf_vqdot_vx_i32m8_tumu(vbool4_t vm, vint32m8_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 1 x i32> [[TMP0]]
 //
 vint32mf2_t test_sf_vqdot_vx_i32mf2_mu(vbool64_t vm, vint32mf2_t vd,
-                                       vint8mf2_t vs2, int32_t rs1,
+                                       vint8mf2_t vs2, uint32_t rs1,
                                        size_t vl) {
   return __riscv_sf_vqdot_mu(vm, vd, vs2, rs1, vl);
 }
@@ -200,7 +200,7 @@ vint32mf2_t test_sf_vqdot_vx_i32mf2_mu(vbool64_t vm, vint32mf2_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 2 x i32> [[TMP0]]
 //
 vint32m1_t test_sf_vqdot_vx_i32m1_mu(vbool32_t vm, vint32m1_t vd,
-                                     vint8m1_t vs2, int32_t rs1, size_t vl) {
+                                     vint8m1_t vs2, uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_mu(vm, vd, vs2, rs1, vl);
 }
 
@@ -211,7 +211,7 @@ vint32m1_t test_sf_vqdot_vx_i32m1_mu(vbool32_t vm, vint32m1_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 4 x i32> [[TMP0]]
 //
 vint32m2_t test_sf_vqdot_vx_i32m2_mu(vbool16_t vm, vint32m2_t vd,
-                                     vint8m2_t vs2, int32_t rs1, size_t vl) {
+                                     vint8m2_t vs2, uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_mu(vm, vd, vs2, rs1, vl);
 }
 
@@ -222,7 +222,7 @@ vint32m2_t test_sf_vqdot_vx_i32m2_mu(vbool16_t vm, vint32m2_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 8 x i32> [[TMP0]]
 //
 vint32m4_t test_sf_vqdot_vx_i32m4_mu(vbool8_t vm, vint32m4_t vd,
-                                     vint8m4_t vs2, int32_t rs1, size_t vl) {
+                                     vint8m4_t vs2, uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_mu(vm, vd, vs2, rs1, vl);
 }
 
@@ -233,6 +233,6 @@ vint32m4_t test_sf_vqdot_vx_i32m4_mu(vbool8_t vm, vint32m4_t vd,
 // CHECK-RV64-NEXT:    ret <vscale x 16 x i32> [[TMP0]]
 //
 vint32m8_t test_sf_vqdot_vx_i32m8_mu(vbool4_t vm, vint32m8_t vd,
-                                     vint8m8_t vs2, int32_t rs1, size_t vl) {
+                                     vint8m8_t vs2, uint32_t rs1, size_t vl) {
   return __riscv_sf_vqdot_mu(vm, vd, vs2, rs1, vl);
 }
