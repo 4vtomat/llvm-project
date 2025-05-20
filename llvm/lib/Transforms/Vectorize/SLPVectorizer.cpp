8f58bf0ba922e66b775b84820b963b7803b24c80
Alexey Bataev <alexey.bataev@sifive.com>
2025-05-20 11:04:29 -0700

    [SiFive][Merge]Cherry-pick ead9d6a56d764190aa916c5259ab50986f803128 from upstream

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -8911,7 +8911,11 @@ void BoUpSLP::buildTree_rec(ArrayRef<Value *> VL, unsigned Depth,
           TTI.getArithmeticInstrCost(Opcode1, Op2VecTy, Kind);
       InstructionCost NewCost =
           NewVecOpsCost + InsertCost +
+#if SIFIVE_CUSTOMIZATION
+          (!VectorizableTree.empty() && VectorizableTree.front()->hasState() &&
+#else
           (VectorizableTree.front()->hasState() &&
+#endif // SIFIVE_CUSTOMIZATION
                    VectorizableTree.front()->getOpcode() == Instruction::Store
                ? NewShuffleCost
                : 0);
