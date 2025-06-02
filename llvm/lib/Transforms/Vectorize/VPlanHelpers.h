5ef3d3d4eb5a651c51245ac4bb583cd72fe0a4dc
Hank Chang <hchang@sw02.internal.sifive.com>
2025-06-01 19:36:00 -0700

    [SiFive][Merge] Resolve conflict in llvm/lib/Transforms/Vectorize/VPlanHelpers.h

--- llvm/lib/Transforms/Vectorize/VPlanHelpers.h
+++ llvm/lib/Transforms/Vectorize/VPlanHelpers.h
@@ -16,13 +16,7 @@
 #define LLVM_TRANSFORMS_VECTORIZE_VPLANHELPERS_H
 
 #include "VPlanAnalysis.h"
-<<<<<<< HEAD
-#if SIFIVE_CUSTOMIZATION
 #include "VPlanDominatorTree.h"
-#endif // SIFIVE_CUSTOMIZATION
-=======
-#include "VPlanDominatorTree.h"
->>>>>>> e45090e5f0bf7743fe0b00d510a903a659354ce1
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/ADT/SmallVector.h"
@@ -437,19 +431,14 @@ struct VPTransformState {
   /// VPlan-based type analysis.
   VPTypeAnalysis TypeAnalysis;
 
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
-  /// VPlan-based dominator tree.
-  VPDominatorTree VPDT;
-
   /// True if the RISCV specific implementation of CSA vectorization is
   /// enabled.
   bool EnableRISCVCSA;
 #endif // SIFIVE_CUSTOMIZATION
-=======
+  
   /// VPlan-based dominator tree.
   VPDominatorTree VPDT;
->>>>>>> e45090e5f0bf7743fe0b00d510a903a659354ce1
 };
 
 /// Struct to hold various analysis needed for cost computations.
