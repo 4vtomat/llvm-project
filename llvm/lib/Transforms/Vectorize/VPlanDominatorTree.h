8ba1261f9ed4b4f4e2008d0b7df354e2e24e8f23
Hank Chang <hchang@sw02.internal.sifive.com>
2025-06-01 19:35:21 -0700

    [SiFive][Merge] Resolve conflict in llvm/lib/Transforms/Vectorize/VPlanDominatorTree.h

--- llvm/lib/Transforms/Vectorize/VPlanDominatorTree.h
+++ llvm/lib/Transforms/Vectorize/VPlanDominatorTree.h
@@ -39,21 +39,10 @@ class VPDominatorTree : public DominatorTreeBase<VPBlockBase, false> {
 
 public:
   VPDominatorTree() = default;
-<<<<<<< HEAD
-#if SIFIVE_CUSTOMIZATION
-  explicit VPDominatorTree(VPlan &Plan) { recalculate(Plan); }
-#endif // SIFIVE_CUSTOMIZATION
-
-  /// Returns true if \p A properly dominates \p B.
-#if SIFIVE_CUSTOMIZATION
-  using Base::properlyDominates;
-#endif // SIFIVE_CUSTOMIZATION
-=======
   explicit VPDominatorTree(VPlan &Plan) { recalculate(Plan); }
 
   /// Returns true if \p A properly dominates \p B.
   using Base::properlyDominates;
->>>>>>> e45090e5f0bf7743fe0b00d510a903a659354ce1
   bool properlyDominates(const VPRecipeBase *A, const VPRecipeBase *B);
 };
 
