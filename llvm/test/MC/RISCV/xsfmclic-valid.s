cc128bfad616b4dffe7b0ba17b61011044a3ddea
Piyou Chen <piyou.chen@sifive.com>
2025-07-27 20:36:05 -0700

    [SiFive][Merge] update llvm/test/MC/RISCV/xsfmclic-valid.s

--- llvm/test/MC/RISCV/xsfmclic-valid.s
+++ llvm/test/MC/RISCV/xsfmclic-valid.s
@@ -10,31 +10,31 @@
 # RUN:     | llvm-objdump -d --mattr=+experimental-xsfmclic -M no-aliases - \
 # RUN:     | FileCheck -check-prefix=CHECK-INST %s
 
-# CHECK-INST: csrrs t1, sf.mtvt, zero
+# CHECK-INST: csrrs t1, mtvt, zero
 # CHECK-ENC: encoding: [0x73,0x23,0x70,0x30]
 csrrs t1, sf.mtvt, zero
-# CHECK-INST: csrrs t2, sf.mtvt, zero
+# CHECK-INST: csrrs t2, mtvt, zero
 # CHECK-ENC: encoding: [0xf3,0x23,0x70,0x30]
 csrrs t2, 0x307, zero
 
-# CHECK-INST: csrrs t1, sf.mnxti, zero
+# CHECK-INST: csrrs t1, mnxti, zero
 # CHECK-ENC: encoding: [0x73,0x23,0x50,0x34]
 csrrs t1, sf.mnxti, zero
-# CHECK-INST: csrrs t2, sf.mnxti, zero
+# CHECK-INST: csrrs t2, mnxti, zero
 # CHECK-ENC: encoding: [0xf3,0x23,0x50,0x34]
 csrrs t2, 0x345, zero
 
-# CHECK-INST: csrrs t1, sf.mintstatus, zero
+# CHECK-INST: csrrs t1, mintstatus, zero
 # CHECK-ENC: encoding: [0x73,0x23,0x60,0x34]
 csrrs t1, sf.mintstatus, zero
-# CHECK-INST: csrrs t2, sf.mintstatus, zero
+# CHECK-INST: csrrs t2, mintstatus, zero
 # CHECK-ENC: encoding: [0xf3,0x23,0x60,0x34]
 csrrs t2, 0x346, zero
 
-# CHECK-INST: csrrs t1, sf.mscratchcsw, zero
+# CHECK-INST: csrrs t1, mscratchcsw, zero
 # CHECK-ENC: encoding: [0x73,0x23,0x80,0x34]
 csrrs t1, sf.mscratchcsw, zero
-# CHECK-INST: csrrs t2, sf.mscratchcsw, zero
+# CHECK-INST: csrrs t2, mscratchcsw, zero
 # CHECK-ENC: encoding: [0xf3,0x23,0x80,0x34]
 csrrs t2, 0x348, zero
 
