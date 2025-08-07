d4c75768801f4dbb3aa872d92158dce6efdeb058
Piyou Chen <piyou.chen@sifive.com>
2025-08-06 21:08:14 -0700

    [SiFive][Merge] Resolve llvm/lib/Target/RISCV/RISCVSubtarget.h

--- llvm/lib/Target/RISCV/RISCVSubtarget.h
+++ llvm/lib/Target/RISCV/RISCVSubtarget.h
@@ -121,14 +121,10 @@ private:
   bool ATTRIBUTE = DEFAULT;
 #include "RISCVGenSubtargetInfo.inc"
 
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
   uint8_t VectorToScalarBaseCost = 1;
-  unsigned XSfmmTE = 0;
 #endif // SIFIVE_CUSTOMIZATION
-=======
   unsigned XSfmmTE = 0;
->>>>>>> d45031ce5281b9fae54f2fdf5edff831e1308976
   unsigned ZvlLen = 0;
   unsigned RVVVectorBitsMin;
   unsigned RVVVectorBitsMax;
