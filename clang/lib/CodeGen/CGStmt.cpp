6a939bc8f2a564ac75a5e0af8699ec9cdb046288
Piyou Chen <piyou.chen@sifive.com>
2025-08-06 21:08:07 -0700

    [SiFive][Merge] Resolve clang/lib/CodeGen/CGStmt.cpp

--- clang/lib/CodeGen/CGStmt.cpp
+++ clang/lib/CodeGen/CGStmt.cpp
@@ -1130,16 +1130,14 @@ void CodeGenFunction::EmitWhileStmt(const WhileStmt &S,
     if (!Weights && CGM.getCodeGenOpts().OptimizationLevel)
       BoolCondVal = emitCondLikelihoodViaExpectIntrinsic(
           BoolCondVal, Stmt::getLikelihood(S.getBody()));
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
     // Appending Branch_weight, Unpredictable and Profile_count metadata.
-    Builder.CreateCondBr(BoolCondVal, LoopBody, ExitBlock, Weights, nullptr,
-                         createProfileCount(getProfileCount(S.getBody())));
+    auto *I =
+        Builder.CreateCondBr(BoolCondVal, LoopBody, ExitBlock, Weights, nullptr,
+                             createProfileCount(getProfileCount(S.getBody())));
 #else
-    Builder.CreateCondBr(BoolCondVal, LoopBody, ExitBlock, Weights);
-#endif // SIFIVE_CUSTOMIZATION
-=======
     auto *I = Builder.CreateCondBr(BoolCondVal, LoopBody, ExitBlock, Weights);
+#endif // SIFIVE_CUSTOMIZATION
     // Key Instructions: Emit the condition and branch as separate source
     // location atoms otherwise we may omit a step onto the loop condition in
     // favour of the `while` keyword.
@@ -1148,7 +1146,6 @@ void CodeGenFunction::EmitWhileStmt(const WhileStmt &S,
     if (auto *CondI = dyn_cast<llvm::Instruction>(BoolCondVal))
       addInstToNewSourceAtom(CondI, nullptr);
     addInstToNewSourceAtom(I, nullptr);
->>>>>>> d45031ce5281b9fae54f2fdf5edff831e1308976
 
     if (ExitBlock != LoopExit.getBlock()) {
       EmitBlock(ExitBlock);
@@ -1260,23 +1257,17 @@ void CodeGenFunction::EmitDoStmt(const DoStmt &S,
   // As long as the condition is true, iterate the loop.
   if (EmitBoolCondBranch) {
     uint64_t BackedgeCount = getProfileCount(S.getBody()) - ParentCount;
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
     // Appending Branch_weight, Unpredictable and Profile_count metadata.
-    Builder.CreateCondBr(
+    auto *I = Builder.CreateCondBr(
         BoolCondVal, LoopBody, LoopExit.getBlock(),
         createProfileWeightsForLoop(S.getCond(), BackedgeCount), nullptr,
         createProfileCount(getProfileCount(S.getBody())));
 #else
-    Builder.CreateCondBr(
-        BoolCondVal, LoopBody, LoopExit.getBlock(),
-        createProfileWeightsForLoop(S.getCond(), BackedgeCount));
-#endif // SIFIVE_CUSTOMIZATION
-=======
     auto *I = Builder.CreateCondBr(
         BoolCondVal, LoopBody, LoopExit.getBlock(),
         createProfileWeightsForLoop(S.getCond(), BackedgeCount));
-
+#endif // SIFIVE_CUSTOMIZATION
     // Key Instructions: Emit the condition and branch as separate source
     // location atoms otherwise we may omit a step onto the loop condition in
     // favour of the closing brace.
@@ -1285,7 +1276,6 @@ void CodeGenFunction::EmitDoStmt(const DoStmt &S,
     if (auto *CondI = dyn_cast<llvm::Instruction>(BoolCondVal))
       addInstToNewSourceAtom(CondI, nullptr);
     addInstToNewSourceAtom(I, nullptr);
->>>>>>> d45031ce5281b9fae54f2fdf5edff831e1308976
   }
 
   LoopStack.pop();
