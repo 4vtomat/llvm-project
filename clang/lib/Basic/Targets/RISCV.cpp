37788f31e15e25b883e554fe68dd9a069738ee20
Piyou Chen <piyou.chen@sifive.com>
2025-09-11 00:09:59 -0700

    [SiFive][Merge] Resolve clang/lib/Basic/Targets/RISCV.cpp

--- clang/lib/Basic/Targets/RISCV.cpp
+++ clang/lib/Basic/Targets/RISCV.cpp
@@ -237,17 +237,13 @@ void RISCVTargetInfo::getTargetDefines(const LangOptions &Opts,
   // Currently we support the v1.0 RISC-V V intrinsics.
   Builder.defineMacro("__riscv_v_intrinsic", Twine(getVersionValue(1, 0)));
 
-<<<<<<< HEAD
 #if SIFIVE_CUSTOMIZATION
   StringRef SiFiveRecodeTarget = getTargetOpts().SiFiveRecode;
   if (SiFiveRecodeTarget == "neon")
     Builder.defineMacro("__sifive_recode_neon");
 #endif // SIFIVE_CUSTOMIZATION
 
-  auto VScale = getVScaleRange(Opts, false);
-=======
   auto VScale = getVScaleRange(Opts, ArmStreamingKind::NotStreaming);
->>>>>>> 77914c96dfc55562404d18c1ab777137055679db
   if (VScale && VScale->first && VScale->first == VScale->second)
     Builder.defineMacro("__riscv_v_fixed_vlen",
                         Twine(VScale->first * llvm::RISCV::RVVBitsPerBlock));
